5c03ac2aec0725e5843159820cb7ba9fbfbe3817
lee <aksndk123@naver.com>
2021-01-05 10:35:24 +0900

    2)계수 3)set이용 1)이진탐색을 이용한 풀이

--- python_book(이것이 코딩테스트다)/07 이진탐색/7-1 부품찾기.py
+++ python_book(이것이 코딩테스트다)/07 이진탐색/7-1 부품찾기.py
@@ -1,24 +1,26 @@
-def binary_search(L, target, start, end):
-    if start > end:
-        return -1
-    mid = (start + end) // 2
-    if L[mid] == target:
-        return mid
-    elif L[mid] < target:
-        start = mid + 1
-    elif L[mid] > target:
-        end = mid - 1
-    return binary_search(L, target, start, end)
+# def binary_search(L, target, start, end):
+#     if start > end:
+#         return -1
+#     mid = (start + end) // 2
+#     if L[mid] == target:
+#         return mid
+#     elif L[mid] < target:
+#         start = mid + 1
+#     elif L[mid] > target:
+#         end = mid - 1
+#     return binary_search(L, target, start, end)
 
 
 n = int(input())
-N = list(map(int, input().split()))
+L = [0]*1000000
+for i in input().split():
+    L[int(i)] = 1
 m = int(input())
 M = list(map(int, input().split()))
-N.sort()
 result = []
 for i in M:
-    if binary_search(N,i,0,n-1) != -1:
+    # if binary_search(N,i,0,n-1) != -1:
+    if L[i] == 1:
         result.append("yes")
     else:
         result.append("no")
