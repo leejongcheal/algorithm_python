c64ca415ed02bab4866e24012354df757eac7bda
lee <aksndk123@naver.com>
2021-02-01 11:58:19 +0900

    람다 정렬 L.sort(key=lambda x: int(x[1]),-(int(x[2])),x[0]) 처럼 사용가능

---
+++ python_book(이것이 코딩테스트다)/14 정렬문제/1 국영수.py
@@ -0,0 +1,7 @@
+N = int(input())
+L = []
+for _ in range(N):
+    L.append(list(input().split()))
+L.sort(key=lambda x:-(int(x[1]), (int(x[2])), (-int(x[3])), x[0]))
+for i in range(N):
+    print(L[i][0])
