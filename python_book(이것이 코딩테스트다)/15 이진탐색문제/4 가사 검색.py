924c8d45e1ddb6e18db35ed85327c60e52de6b06
lee <aksndk123@naver.com>
2021-02-02 13:54:50 +0900

    NlogN -> 탐색중 이진탐색 -> 이진탐색의 기본은 정렬후 -> 뭐를 정렬하고 뭐를 기준으로 이진탐색?????
    기본적인 라이브러리 암기 // 리스트와 문자열의 주옥같은 미세 차이 라이브러리 사용

---
+++ python_book(이것이 코딩테스트다)/15 이진탐색문제/4 가사 검색.py
@@ -0,0 +1,32 @@
+from bisect import bisect_left, bisect_right
+
+def solution(words, queries):
+    answer = []
+    word_by_len = [[] for _ in range(11000)]
+    reverse_by_len = [[] for _ in range(11000)]
+    for w in words:
+        word_by_len[len(w)].append(w)
+        reverse_by_len[len(w)].append(w[::-1])
+    for w in range(len(word_by_len)):
+        word_by_len[w].sort()
+        reverse_by_len[w].sort()
+    for q in queries:
+        len_q = len(q)
+        # 거꾸로
+        cnt = 0
+        if q[0] == '?':
+            rq = q[::-1]
+            ra = rq.replace('?',"a")
+            rz = rq.replace('?',"z")
+            cnt = bisect_right(reverse_by_len[len_q],rz) - bisect_left(reverse_by_len[len_q], ra)
+        elif q[0] != '?':
+            qa= q.replace('?',"a")
+            qz= q.replace('?','z')
+            cnt = bisect_right(word_by_len[len_q], qz) - bisect_left(word_by_len[len_q], qa)
+        answer.append(cnt)
+    return answer
+
+
+words = list(input().split())
+queries = list(input().split())
+print(solution(words,queries))
