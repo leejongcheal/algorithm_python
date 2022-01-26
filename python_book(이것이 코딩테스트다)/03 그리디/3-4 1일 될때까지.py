a26a4f711fc3795789f9f51f088e0f21cf9f8b03
lee <aksndk123@naver.com>
2022-01-26 16:12:40 +0900

    다시 전의 풀이로 바꿈 이유는 N이 작을떄는 괜찮은데 억단위일떄는 -1부분과 연속으로 나누는 부분을 줄여서 문제를 풀어야 하는경우까지 생각
    N단위에 따라서 선택해서 사용하면된다.

--- python_book(이것이 코딩테스트다)/03 그리디/3-4 1일 될때까지.py
+++ python_book(이것이 코딩테스트다)/03 그리디/3-4 1일 될때까지.py
@@ -1,8 +1,15 @@
-N, K = map(int, input().split())
+n, k = map(int, input().split())
 cnt = 0
-while N != 1:
-    if N % K == 0:
-        N //= K
-    else : N -= 1
-    cnt += 1
-print(cnt)
+while True:
+    # 현재 나머지값을 n에 빼줘서 k의 배수로 만듬
+    remainder = n % k
+    cnt += remainder
+    n -= remainder
+    # n을 k로 나눠주고 나머지를 뺴주는 과정을 0이 될때까지 반복
+    if n != 0:
+        n //= k
+        cnt += 1
+    else:
+        break
+# 1까지가 목적인데 조건은 0으로 줬으니 count의 갯수를 하나 뺀다.
+print(cnt - 1)
