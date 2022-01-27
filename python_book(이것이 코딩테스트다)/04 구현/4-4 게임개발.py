8db2237c1ab51fc11c1f113b773a625ce18a1e54
lee <aksndk123@naver.com>
2022-01-27 11:25:11 +0900

    다시 풀어서 다른 풀이 ++ 풀면서 느낀 실수정리

--- python_book(이것이 코딩테스트다)/04 구현/4-4 게임개발.py
+++ python_book(이것이 코딩테스트다)/04 구현/4-4 게임개발.py
@@ -1,42 +1,36 @@
-n, m = map(int, input().split())
-A, B, d = map(int, input().split())
-# 0  1  2  3
-# 북 동 남 서 -> 왼족 서 북 동 남 // 반시계라 거꾸로 가면된다. -1 씩 해주면됨
-steps = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-result = 1
-L = []
-for _ in range(n):
-    L.append(list(map(int, input().split())))
-# 0 육지 1 바다
-visit = [[0] * m for _ in range(n)]
-visit[A][B] = 1
-while 1:
-    move = 0
-    # 각 방향 왼쪽 조사
-    for i in range(4):
-        nd = (d - (i+1))%4
-        na = A + steps[nd][0]
-        nb = B + steps[nd][1]
-        # 이동조건  1) 칸안 2) 방문한적 없는 3) 육지 해당 방향으로 이동
-        if 0 <= na < n and 0 <= nb < m and L[na][nb] == 0 and visit[na][nb] == 0:
-            A, B, d = na, nb, nd
-            # print(A,B,d,"쪽 이동")
-            visit[A][B] = 1
-            move = 1
-            result += 1
+"""
+구현중에 쉬운편인데 확실히 구현은 문제이해랑 예외체크에 신경을 많이 써야하는거 같다. 몇번을 틀렸는지
+4방향 다확인후에 뒤로가는것도 3방향만 보고 뒤로가는걸로 그냥 넘겨버리고 1,1시작이 -> 0,0 시작을 다르게 말하는줄로 알아먹고
+nd 와 d를 써야하는 경우가 있는데 흐름이 아닌 습관적으로 nd만 계속쓰는등 확실히 피지컬이 많이 딸리는것을 많이 느낌
+개같다 진짜로..
+"""
+steps = [(-1, 0),(0 , 1),(1, 0),(0, -1)]
+N, M = map(int, input().split())
+x, y, d = map(int, input().split())
+L = [list(map(int, input().split())) for _ in range(N)]
+count = 1
+L[x][y] = 2
+flag = 1 # 회전후 이동 or 뒤로 간경우 1로 둘것
+while flag:
+    flag = 0
+    # 회전후 이동하는지 확인
+    for i in range(1, 5): # 1  2 3 만 확인 -> 4도 확인해야 ..
+        nd = ( d + i) % 4
+        nx, ny = x + steps[nd][0], y + steps[nd][1]
+        if 0 <= nx < N and 0 <= ny < M and L[nx][ny] == 0:
+            flag = 1
+            count += 1
+            L[nx][ny] = 2
+            print(x, y, d, nx, ny, nd, "회전후 이동")
+            x, y, d = nx, ny, nd
             break
-    # 이동이 안되서 뒤로 가는 경우
-    if move == 0:
-        na = A - steps[d][0]
-        nb = B - steps[d][1]
-        # 뒤로가는 조건 1) 칸안 2) 육지
-        if 0 <= na < n and 0 <= nb < m and L[na][nb] == 0:
-            A, B = na, nb
-            # print(A,B,d,"뒤로이동후 좌표")
-        # 멈추는 조건 1) 바다 2) 칸밖
-        else:
-            # print(A,B,d,"멈춤 좌표")
-            break
-# for i in visit:
-#     print(i)
-print(result)
+    # 다 회전후에 뒤로 갈수 있는지 확인
+    if flag == 0: # 회전후 이동 안한 경우
+        nx, ny = x - steps[d][0], y - steps[d][1]
+        if 0 <= nx < N and 0 <= ny < M and L[nx][ny] != 1:
+            flag = 1
+            print(x,y,nx, ny,"뒤이동")
+            x, y = nx, ny
+print(count)
+
+
