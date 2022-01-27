d7e5f9af0f735f4b1711ce784edffafead49904b
lee <aksndk123@naver.com>
2022-01-27 13:24:32 +0900

    001처럼 뛰어쓰기가 없는 입력값은 split를 사용하지않는다 주석 확인

--- python_book(이것이 코딩테스트다)/05 DFS BFS/3-1 음료슈 얼려먹기.py
+++ python_book(이것이 코딩테스트다)/05 DFS BFS/3-1 음료슈 얼려먹기.py
@@ -1,3 +1,10 @@
+"""
+001, 010, 100 같은것은 list(map(int, input().split()))로 받으면
+[1], [10], [100]의 값을 가진다. 띄어 쓰기가 없는경우에는 input().split()에 대해서 list화 하기보다는
+input()자체에 list화 를 한다. 위의 경우 list(map(int, input()))을 사용한다.
+input()자체에 리스트화를 하면 한글자 한글자 원소화 시켜서 값을 저장한다.
+ex) asd -> ['a', 's', 'd'] // a s d -> ['a', ' ', 's', ' ', 'd']
+"""
 from _collections import deque
 
 n, m = map(int, input().split())
