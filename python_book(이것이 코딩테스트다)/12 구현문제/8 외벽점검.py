f5c00608b5e4dfe6e938fa50a9be7366c802f326
lee <aksndk123@naver.com>
2022-01-30 00:48:19 +0900

    ******* perm을 줄이는 쪽으로 알고리즘을 짰어야하는데 나는 인간수를 1부터 큰순으로 weak에 대해서 perm 연산해서 다 커버를 한다면 지금 인간수가 최소수 이므로 이것을 답으로 하는식으로 알고리즘을 짜서 시간초과 받음
    -> perm 연산은 최대한 줄일수 있는 방향 a->b // b -> a중 뭐로 할질 고민하자

--- python_book(이것이 코딩테스트다)/12 구현문제/8 외벽점검.py
+++ python_book(이것이 코딩테스트다)/12 구현문제/8 외벽점검.py
@@ -1,30 +1,86 @@
 from itertools import permutations
 def solution(n, weak, dist):
-    length = len(weak)
-    answer = n + 1
-    for i in range(length):
-        weak.append(weak[i] + n)
-    for start_index in range(length):
-        for friends in list(permutations(dist,len(dist))):
-            cnt = 1
-            # 처음 친구의 최대위치
-            position = weak[start_index] + friends[cnt - 1]
-            # 어디 까지 가는지 검사
-            for i in range(start_index, start_index + length):
-                # 더갈수 있음
-                if position >= weak[i]:
-                    continue
-                # 못간곳에서 친구 떨어질떄까지 더해서 가능한지 보기
-                else:
-                    cnt += 1
-                    if cnt > len(friends):
+    W = len(weak)
+    D = len(dist)
+    dist.sort(reverse=True)
+    weak2 = []
+    res = int(1e10)
+    # 커버되는곳은 1로 두어서 나중에 연속1의 갯수가 W개 나오는지를 확인해서 다커버하는지 확인
+    check_weak2 = [0]*2*n
+    for i in range(len(weak)):
+        weak2.append(weak[i])
+    for i in range(len(weak)):
+        weak2.append(n + weak[i])
+    for dist_list in permutations(dist, D):
+        for start_index in range(W):
+            now = start_index
+            flag = 0
+            check_weak2 = [0]*W
+            for i in range(len(dist_list)):
+                start = weak2[now]
+                end = weak2[now] + dist_list[i]
+                for j in range(now, 2*W):
+                    if start <= weak2[j] <= end:
+                        check_weak2[j%W] = 1
+                        if check_weak2.count(1) == W:
+                            res = min(res, i + 1)
+                            flag = 1
+                    else:
+                        now = j
                         break
-                    position = weak[i] + friends[cnt - 1]
-            answer = min(answer, cnt)
-    if answer > len(dist):
-        return -1
+                if flag == 1:
+                    break
+    if res < int(1e10):
+        return res
     else:
-        return answer
+        return -1
+
+"""나는 weak에 대해서 인원을 1~D(W보다 작은값에 대해서) prem을 구한다음에
+친구리스트는 그대로해서 넣어서 다 커버하는지 확인을 했는데 시간초과가 뜸
+
+답지는 dist에 대해서 D의 갯수로 perm 한번 구한다음에 이걸로 시작점을 0~ W-1 시작으로 두어서
+구한 dist_list로 다 채우는지확인해서 채우고나서 min cnt를 구하는 방식으로 푸니 시간이 훨씬 단축됨
+--> 애매한 문제
+
+통과는 했는데 내가 볼땐 check_weak2 부분에서 시간이 생각보다 오래걸리는듯?
+-> 밑의 코드처럼 weak에 대해서 1~W순서로 perm 해서 푸니깐 어쩌피 시간초과임 perm자체를 줄이는 식으로
+알고리즘을 짯어야함
+"""
+
+# from itertools import permutations
+# def solution(n, weak, dist):
+#     W = len(weak)
+#     D = len(dist)
+#     dist.sort(reverse=True)
+#     weak2 = []
+#     # 커버되는곳은 1로 두어서 나중에 연속1의 갯수가 W개 나오는지를 확인해서 다커버하는지 확인
+#     check_weak2 = [0]*2*n
+#     for i in range(len(weak)):
+#         weak2.append(weak[i])
+#     for i in range(len(weak)):
+#         weak2.append(n + weak[i])
+#     for cnt in range(1, D+1):#D + 1):
+#         # W < D 인경우 1~W-1 까지 해서 못구했으면 W명 넣으면 무조건 커버하니 답
+#         if cnt == W:
+#             return cnt
+#         for weak_list in permutations(weak, cnt):
+#             # print(cnt, list(weak_list))
+#             di = 0
+#             check_weak2 = [0] * n
+#             for w in list(weak_list):
+#                 start = w
+#                 end = w + dist[di]
+#                 for i in range(2*W):
+#                     if start <= weak2[i] <= end:
+#                         check_weak2[i % W] = 1
+#                         if check_weak2.count(1) == W:
+#                             return cnt
+#                 di += 1
+#     return -1
+
+
+
+
 
 
 
