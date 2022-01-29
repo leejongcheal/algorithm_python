106fdeaa0410dd2bfbe6d9d688e60b7e0bfd5c7b
lee <aksndk123@naver.com>
2022-01-29 16:43:17 +0900

    전의 풀이는 뭔가 드럽게 풀어서 길긴하지만 쉬운 알고리즘으로 푼것으로 답안 바꿈 숫자가 1개가 아닌 십의 자리 이상인 경우 갯수가 1개가 아닌 2개로 본다는것이 중요하고, 굳이 갯수를 구하기보다는 s=""해서 만들어서 갯수 세는것이 예외처리할필요 없이 매우편함

--- python_book(이것이 코딩테스트다)/12 구현문제/3 문자열 압축.py
+++ python_book(이것이 코딩테스트다)/12 구현문제/3 문자열 압축.py
@@ -1,43 +1,32 @@
-INF = int(1e9)
 def solution(s):
-    l = len(s)
-    # 0 인경우 무시할려고 INF 넣어줌
-    res = [INF]*(l//2 + 10)
-    # 3이하인경우 하나씩만 가능해서 자신을 리턴 \
-    if l <= 1:
-        return l
-    # 1부터 l//2 까지 하나하나 계산후 최솟값 반환
-    for i in range(1,l//2+1):
-        num = i
-        index = 0
-        set_s = s[index:index+i]
-        index += i
-        flag = 0
-        # 끝 or 끝의 전까지만 계산 나중에 예외처리로 더해줌
-        while index + i <= l:
-            # 같은경우 몇개 같은지 flag에 계산
-            if set_s == s[index:index+i]:
-                flag += 1
-            # 다른경우 새로운 쌍의 문자열수를 넣어주고 set_s 갱신
+    res = len(s)
+    if res < 2:
+        return res
+    for seq in range(1, len(s)//2+1):
+        temp_s = ""
+        pre = s[0:seq]
+        r = len(s) // seq
+        cnt = 1
+        for i in range(1,r):
+            now = s[i*seq:(i+1)*seq]
+            if now == pre:
+                cnt += 1
             else:
-                # 달라진 경우 숫자의 자릿수 만큼 num 에 넣어주기
-                if flag != 0:
-                    num += len(str(flag + 1))
-                flag = 0
-                num += i
-                set_s = s[index:index+i]
-            index += i
-        # 마지막에 계속 같게온 경우 flag에 대해서 계산해줘야함
-        if flag != 0:
-            num += len(str(flag + 1))
-        # 딱 떨어지는경우
-        if index == l:
-            res[i] = num
-        # 끝에 남는 경우 하나씩 더해줌
+                if cnt != 1:
+                    temp_s += pre+str(cnt)
+                    cnt = 1
+                else:
+                    temp_s += pre
+                    cnt = 1
+            pre = now
+        # 끝나고 나서 남은 pre에 대해서도 처리해주기
+        if cnt != 1:
+            temp_s += pre+str(cnt)
         else:
-            res[i] = num + l - index
-    return min(res)
-
-
+            temp_s += pre
+        # 나머지에 대해서 붙여주기
+        temp_s += s[r*seq:len(s)]
+        res = min(res,len(temp_s))
+    return res
 s = input()
 print(solution(s))
