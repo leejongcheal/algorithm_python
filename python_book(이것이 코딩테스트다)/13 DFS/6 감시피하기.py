0f01ab3b994495650ff6629a34317bd3691e29d1
lee <aksndk123@naver.com>
2022-01-31 01:06:53 +0900

    조합이 아닌 DFS로 푸는게 맞음 DFS풀이로 수정

--- python_book(이것이 코딩테스트다)/13 DFS/6 감시피하기.py
+++ python_book(이것이 코딩테스트다)/13 DFS/6 감시피하기.py
@@ -1,60 +1,44 @@
-steps = [(1,0),(-1,0),(0,1),(0,-1)]
-def check(T_list, Map):
-    global N
-    for x, y in T_list:
+def check(Map):
+    global N, T
+    steps = [(1,0),(-1,0),(0,1),(0,-1)]
+    for tx, ty in T:
         for dx, dy in steps:
-            for i in range(1,N):
-                nx, ny = x + dx*i, y + dy*i
-                if 0 <= nx < N and 0 <= ny < N:
-                    if Map[nx][ny] == "S":
-                        return 0
-                    elif Map[nx][ny] == "O":
-                        break
-                else:
+            x, y = tx + dx, ty + dy
+            while 0 <= x < N and 0 <= y < N:
+                if Map[x][y] == "O":
                     break
+                if Map[x][y] == "S":
+                    return 0
+                x, y = x + dx, y + dy
     return 1
 
-
-def comb(L, N):
-    length = len(L)
-    if length < N:
-        return ""
-    ret = []
-    if N == 1:
-        for l in L:
-            ret.append([l])
+def dfs(count):
+    global Map, N, flag
+    if flag == 1:
+        return
+    if count == 3:
+        if check(Map):
+            flag = 1
     else:
-        for i in range(length - N + 1):
-            for temp in comb(L[i+1:], N-1):
-                ret.append([L[i]] + temp)
-    return ret
+        for i in range(N):
+            for j in range(N):
+                if Map[i][j] == "X":
+                    Map[i][j] = "O"
+                    dfs(count + 1)
+                    Map[i][j] = "X"
+
 
 
 N = int(input())
-Map = []
-for _ in range(N):
-    Map.append(list(input().split()))
-X_list = []
-T_list = []
+Map = [list(input().split()) for _ in range(N)]
+T = []
 for i in range(N):
     for j in range(N):
         if Map[i][j] == "T":
-            T_list.append((i, j))
-        if Map[i][j] == "X":
-            X_list.append((i,j))
+            T.append((i,j))
+# flag = 1 이면 가능하다는 뜻
 flag = 0
-for O_list in comb(X_list, 3):
-    if flag == 1:
-        break
-    for i in range(3):
-        x, y = O_list[i]
-        Map[x][y] = "O"
-    if check(T_list, Map):
-        flag = 1
-        break
-    for i in range(3):
-        x, y = O_list[i]
-        Map[x][y] = "X"
+dfs(0)
 if flag == 1:
     print("YES")
 else:
