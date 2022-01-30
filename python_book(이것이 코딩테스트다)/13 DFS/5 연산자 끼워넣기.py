73f3cc4cd35e5364d8e3aee733b8b11be5680435
lee <aksndk123@naver.com>
2022-01-31 00:43:28 +0900

    조합이 아닌 BFS풀이가 아닌 DFS로 풀어야 하는 문제 조합 자체는 왠만하면 DFS로 해결이 된다 + 보면 알겠지만 visit부분이 oper list로 해결이 되어서 만약에 BFS로 쓴다면 q에 하나의 갱신된 리스트를 만들어서 넣어줘야해서 메모리 낭비 +  복잡한 코딩인데 DFS의 -1 +1 테크닉으로 편하게 코딩 가능

--- python_book(이것이 코딩테스트다)/13 DFS/5 연산자 끼워넣기.py
+++ python_book(이것이 코딩테스트다)/13 DFS/5 연산자 끼워넣기.py
@@ -1,48 +1,52 @@
-from collections import deque
+"""
+itertool의 permutation 을 써도 통과는됌 -> 그러나 거의 한계시간으로 실패한 알고리즘으로보임
+BFS를 이용해서 풀수도 있음 (2번째 주석부분) 보면 알겠지만 큐에 oper를 카피하고 해당 명령어를
+-1한 리스트값을 q에 넣어서 BFS 돌림 -> 메모리 낭비 + 코딩자체가 까다로움
+DFS풀이를 보면 알겠지만
+plus -= 1
+dfs(i+1,data + L[i])
+plus -= 1
+처럼 간단한 풀이가 가능함
+DFS를 써야하는 경우는 이처럼 visit 함수대신 어떠한 리스트에 대해서 가지고 가야하는 경우
+BFS는 q에 계속 리스트를 만들어서 넣어줘야하지만 DFS는 -1 +1 테크닉으로 편하게 풀이가능
+특히 순열,조합 느낌나는경우는 DFS풀이를 해줘야함
 
-
-def new_oper(oper, index):
-    s = [0] * 4
-    for i in range(4):
-        s[i] = oper[i]
-    s[index] -= 1
-    return s
-
-
-def BFS(N, A, oper):
-    result = []
-    # 계산값 /index위치/ 현재 oper 리스트
-    q = deque()
-    q.append((A[0], 1, oper))
-    while q:
-        res, index, op = q.popleft()
-        if index == N:
-            result.append(res)
-            continue
-        a, b, c, d = op[0], op[1], op[2], op[3]
-        if a != 0:
-            q.append((res + A[index], index + 1, [a - 1, b, c, d]))
-        if b != 0:
-            q.append((res - A[index], index + 1, [a, b - 1, c, d]))
-        if c != 0:
-            q.append((res * A[index], index + 1, [a, b, c - 1, d]))
-        if d != 0:
-            if res < 0 and A[index] > 0:
-                q.append((-1 * ((-1 * res) // A[index]), index + 1, [a, b, c, d - 1]))
-            elif res > 0 and A[index] < 0:
-                q.append((-1 * (res // (-1 * A[index])), index + 1, [a, b, c, d - 1]))
-            else:
-                q.append((res // A[index], index + 1, [a, b, c, d - 1]))
-    return result
+"""
+def dfs(index, now):
+    global L, N, plus, minus, mul, div, Max, Min
+    if index == 0:
+        now = L[index]
+    if index == N:
+        Max = max(Max, now)
+        Min = min(Min, now)
+    else:
+        if plus > 0:
+            plus -= 1
+            dfs(index + 1, now + L[index])
+            plus += 1
+        if minus > 0:
+            minus -= 1
+            dfs(index + 1, now - L[index])
+            minus += 1
+        if mul > 0:
+            mul -= 1
+            dfs(index + 1, now * L[index])
+            mul += 1
+        if div > 0:
+            if L[index] != 0:
+                div -= 1
+                dfs(index + 1, int(now / L[index]))
+                div += 1
 
 
 N = int(input())
-A = list(map(int, input().split()))
-# + - * % 순서
-oper = list(map(int, input().split()))
-result = BFS(N, A, oper)
-# print(len(result))
-print(max(result), min(result))
+L = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+Max = -1*int(1e10)
+Min = int(1e10)
+dfs(1, L[0])
+print(Max)
+print(Min)
 
 ### 람다 사용 BFS약간의 응용 + 계산을 함수로 뺴서 풀이 -> 깔끔
 # def deep_copy(L):
