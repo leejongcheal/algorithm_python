e1a5136a064f18f9762fea345354258c451a2d1d
lee <aksndk123@naver.com>
2022-01-30 15:12:25 +0900

    BFS로 풀이할떄 visit을 안쓰고 풀어도 되는 경우 -> why? 어쩌피 0인 값에 대해서만 확인을 하고 Map에 2로 채워주는거 자체가 visit을 하는 행위이다. 중요한것은 D/B FS문제는 visit이 필요한것은 당연한건데 Map 값을 채워서 visit을 굳이 안하는 경우도 있다는 것을 이해해서 이런 유형이 있을떄 visit은 필요한데 안써도 되는지 고민하면서 풀것 + 메모리 관련해서 주석 확인

--- python_book(이것이 코딩테스트다)/13 DFS/2 연구소.py
+++ python_book(이것이 코딩테스트다)/13 DFS/2 연구소.py
@@ -1,63 +1,78 @@
-from collections import  deque
-def BFS(graph, wall, N , M):
-    steps = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-    for w in wall:
-        x, y = w[0], w[1]
-        graph[x][y] = 1
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 2:
-                q = deque()
-                q.append((i,j))
-                while q:
-                    x ,y = q.popleft()
-                    for step in steps:
-                        nx = x + step[0]
-                        ny = y + step[1]
-                        if 0 <= nx < N and 0 <= ny < M:
-                            if graph[nx][ny] == 0:
-                                graph[nx][ny] = 3
-                                q.append((nx, ny))
+from collections import deque
+
+
+def comb(L, M):
+    N = len(L)
+    if N < M:
+        return None
+    res = []
+    if M == 1:
+        for l in L:
+            res.append([l])
+    else:
+        for i in range(N - M + 1):
+            for temp in comb(L[i + 1:], M - 1):
+                res.append([L[i]] + temp)
+    return res
+
+
+# visit을 안써도 0인 부분에 대해서 검사를  하기떄문에 상관없을듯? 크기도 작고
+# 확인필요 -> 안해도 상관없음
+def BFS(Map, x, y):
+    q = deque()
+    q.append((x, y))
+    global steps, N, M
+    while q:
+        x, y = q.popleft()
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                if Map[nx][ny] == 0:
+                    q.append((nx, ny))
+                    Map[nx][ny] = 2
+
+
+def check(Map):
+    global N, M
     cnt = 0
     for i in range(N):
         for j in range(M):
-            if graph[i][j] == 0:
+            if Map[i][j] == 0:
                 cnt += 1
     return cnt
 
-def comb(L, n):
-    ret = []
-    N = len(L)
-    if len(L) < n:
-        return None
-    if n == 1:
-        for i in L:
-            ret.append([i])
-    else:
-        for i in range(N - n + 1):
-            for temp in comb(L[i+1:], n-1):
-                ret.append([L[i]] + temp)
-    return ret
 
-
-N, M = map(int, input().split())
-result = 0
-graph = []
-#빈칸
-blank = []
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 0:
-            blank.append((i,j))
-com_3_wall = comb(blank, 3)
-g = [[0]*M for _ in range(N)]
-for wall in com_3_wall:
+def copy_list(Map):
+    global N, M
+    # copy_Map을 굳이 계속 새로만들 필요없이 한번만 초기화해주고 Map값을 넣어줘서 사용
+    # 하면 메모리 아낄수 있음
+    # copy_list(Map, copy_Map)으로 두고 메인에서 한번만 copy_Map 초기화
+    copy_Map = [[0] * M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            g[i][j] = graph[i][j]
-    result = max(BFS(g, wall, N, M), result)
-print(result)
+            copy_Map[i][j] = Map[i][j]
+    return copy_Map
 
 
+steps = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+N, M = map(int, input().split())
+Map = [list(map(int, input().split())) for _ in range(N)]
+virus = []
+blank = []
+res = 0
+for i in range(N):
+    for j in range(M):
+        if Map[i][j] == 2:
+            virus.append([i, j])
+        elif Map[i][j] == 0:
+            blank.append([i, j])
+for blank_3 in comb(blank, 3):
+    copy_Map = copy_list(Map)
+    for b in blank_3:
+        x, y = b[0], b[1]
+        copy_Map[x][y] = 1
+    for v in virus:
+        x, y = v[0], v[1]
+        BFS(copy_Map, x, y)
+    res = max(res, check(copy_Map))
+print(res)
