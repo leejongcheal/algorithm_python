139fb07bed35b4fc0da73dc1f8cbf819e95c7873
lee <aksndk123@naver.com>
2022-01-30 14:23:05 +0900

    BFS이니 지금의 q.popleft값이 K 값인경우 K에 대해서 다조사 했으니 break해서 시간 단축시킴 + q의 원소를 (index, dist)로 해서 편하게품 굳이 distance 배열을 사용해서 다구하고 K인 부분을 구해야 하는가 -> visit에 넣어서  사용하는 방법도 있는데 굳이 visit 자체의 기능을 훼손시키기 싫었음

--- python_book(이것이 코딩테스트다)/13 DFS/1 특정 거리의 도시 찾기.py
+++ python_book(이것이 코딩테스트다)/13 DFS/1 특정 거리의 도시 찾기.py
@@ -1,28 +1,30 @@
 from collections import deque
-N, M, K, start = map(int, input().split())
-graph = [[]for _ in range(N+1)]
+
+N, M, K, X = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+res = []
+visit = [0]*(N+1)
+visit[X] = 1
 for _ in range(M):
-    a, b = map(int,input().split())
+    a, b = map(int, input().split())
     graph[a].append(b)
 q = deque()
-q.append(start)
-visit = [0]*(N+1)
-visit[start] = 1
-result = []
-# BFS 시작
+q.append((X, 0))
+visit[X] = 1
 while q:
-    now = q.popleft()
-    if visit[now] == K + 1:
-        result.append(now)
-        continue
-    for conneted_index in graph[now]:
-        if visit[conneted_index] == 0:
-            q.append(conneted_index)
-            visit[conneted_index] = visit[now] + 1
-    # 방문한적없는 경우에 q에 넣어주면 되겠네
-result.sort()
-if len(result) == 0:
-    print(-1)
+    now, dist = q.popleft()
+    if dist == K:
+        break
+    for next in graph[now]:
+        if visit[next] == 0:
+            visit[next] = 1
+            next_dist = dist + 1
+            q.append((next, next_dist))
+            if next_dist == K:
+                res.append(next)
+res.sort()
+if res:
+    for r in res:
+        print(r)
 else:
-    for i in result:
-        print(i)
+    print(-1)
