e38d39f555908794bf572929ae78dd695942cfd6
lee <aksndk123@naver.com>
2022-01-30 20:39:07 +0900

    완전탐색 풀이 시간초과 -> 다른 풀이방법 발상 -> 2차원 리스트 virus을 두어서 visit + Map을 돌면서 해당 바이러스번호 나오면 BFS하는것을 대체한다. ++ 2차원 virus대신 heapq를 사용해서 풀이해도 편함 핵심은 virus[k] 에 대해 전이 시키는 중에 새로 전이된곳을 virus[k]에 추가하면 새로 전이된곳에 대해서 또 전이를 시키기 때문에 원래 viurs[k] + 새로전이된리스트 를 나중에 virus[k]에 넣어주는것이 핵심 -> visit 대체

--- python_book(이것이 코딩테스트다)/13 DFS/3 경쟁적 전염.py
+++ python_book(이것이 코딩테스트다)/13 DFS/3 경쟁적 전염.py
@@ -1,71 +1,53 @@
-def BFS(graph, x, y, k):
-    global N
-    global black_cnt
-    global plus_list
-    steps = [(1,0),(-1,0),(0,1),(0,-1)]
-    for step in steps:
-        nx = x + step[0]
-        ny = y + step[1]
-        if 0 <= nx < N and 0 <= ny < N:
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = k
-                plus_list.append((nx, ny))
-                black_cnt -= 1
+"""
+발상 완전탐색 n^2 * S * K 에서 몇천만 단위 넘음
+virus 리스트를 사용해서 풀이 다돌면서 k인경우 + visit 사용해서 푸는건 N^2의 시간복잡도
 
+완전탐색대신 virus 2차원 리스트를 사용해서 그거에 대해서만 검사후 전이를 시킴
+-> 통과
 
+위의 2차원 리스트 대신 heapq를 사용해서 푸는 방법도 있네 ㅋㅋ
+핵심은 virus에 추가된 인덱스들은 나중에 추가해서 방문된곳을 또 방문해서 전이시키는 것을
+안하는게 중요핵심임 -> visit을 하냐 이거야
+
+"""
+from collections import deque
+
+steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 N, K = map(int, input().split())
-virus_list = [[] for _ in range(K+1)]
-graph = []
-black_cnt = 0
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
+Map = [list(map(int, input().split())) for _ in range(N)]
+visit = [[0] * N for _ in range(N)]
+S, X, Y = map(int, input().split())
+virus = [[] for _ in range(K)]
+blank = N * N
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 0:
-            black_cnt += 1
-        else:
-            virus_list[graph[i][j]].append((i, j))
-S, X, Y = map(int, input().split())
-time = 0
-# 종료조건 타임이 S초 또는 빈칸의 갯수 0개인경우
-while 1:
-    if time == S:
+        if Map[i][j] != 0:
+            blank -= 1
+            virus[Map[i][j] - 1].append((i, j))
+X -= 1
+Y -= 1
+flag = 0
+for time in range(S):
+    # k+1 이 현재 바이러스번호 로 사용하기
+    for k in range(K):
+        temp = []
+        while virus[k]:
+            x, y = virus[k].pop()
+            temp.append((x, y))
+            for dx, dy in steps:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == 0:
+                    Map[nx][ny] = k+1
+                    blank -= 1
+                    temp.append((nx, ny))
+            if blank == 0:
+                flag = 1
+                break
+        virus[k] = temp
+        if flag == 1:
+            break
+    if flag == 1:
         break
-    time += 1
-    for i in range(1, K+1):
-        plus_list = []
-        for x, y in virus_list[i]:
-            BFS(graph, x, y, i)
-        virus_list[i].extend(plus_list)
-    if black_cnt <= 0:
-        break
+        virus[k] = temp
 
-print(graph[X-1][Y-1])
-
-
-## 깔끔풀이
-# N, K = map(int, input().split())
-# graph = []
-# virus = [[] for _ in range(K)]
-# steps = [(1,0),(-1,0),(0,1),(0,-1)]
-# for i in range(N):
-#     graph.append(list(map(int, input().split())))
-# S, X, Y = map(int, input().split())
-# for i in range(N):
-#     for j in range(N):
-#         if graph[i][j] != 0:
-#             virus[graph[i][j]-1].append((i,j))
-# for i in range(S):
-#     for v in range(K):
-#         if len(virus[v]) != 0:
-#             new = []
-#             while virus[v]:
-#                 x,y = virus[v].pop()
-#                 for step in steps:
-#                     nx = x + step[0]
-#                     ny = y + step[1]
-#                     if 0 <= nx < N and  0<= ny < N and graph[nx][ny] == 0:
-#                         graph[nx][ny] = v+1
-#                         new.append((x,y))
-#             virus[v] = new
-# print(graph[X-1][Y-1])
+print(Map[X][Y])
