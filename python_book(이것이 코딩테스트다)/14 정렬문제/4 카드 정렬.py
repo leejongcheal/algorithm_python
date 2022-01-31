705c20dac8b83944c33615abcd184d79a59890e8
lee <aksndk123@naver.com>
2022-01-31 13:53:58 +0900

    문제 이해에서 틀렸음 a + b 한걸 다시 L에 들어가서 sort 가 필요함  -> 위에서 바로 heapq -> 정렬을 유지해야하는 리스트에 값을 계속 집어넣고 뺴야하는 경우에

--- python_book(이것이 코딩테스트다)/14 정렬문제/4 카드 정렬.py
+++ python_book(이것이 코딩테스트다)/14 정렬문제/4 카드 정렬.py
@@ -10,3 +10,8 @@ while len(L) >= 2:
     res += a + b
     heapq.heappush(L,a+b)
 print(res)
+"""
+문제 이해에서 틀렸음 a + b 한걸 다시 L에 들어가서 sort 가 필요함
+ -> 위에서 바로 heapq -> 정렬을 유지해야하는 리스트에 값을 계속 집어넣고 뺴야하는 경우에
+
+"""
