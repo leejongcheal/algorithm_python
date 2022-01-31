4fe1cf38c56c0e246a4504ea9b5e4597c5383424
lee <aksndk123@naver.com>
2022-01-31 17:26:00 +0900

    더 깔끔한 풀이로 + 인풋예시 text 주석으로 추가

--- python_book(이것이 코딩테스트다)/16 다이나믹프로그래밍 문제/1 금광.py
+++ python_book(이것이 코딩테스트다)/16 다이나믹프로그래밍 문제/1 금광.py
@@ -1,35 +1,37 @@
-def solution(result, x, y):
-    global Map, N, M
-    if result[x][y] != -1:
-        return result[x][y]
-    if y == 0:
-        result[x][y] = Map[x][y]
-    else:
-        if x == 0:
-            result[x][y] = max(solution(result, x, y - 1), solution(result, x + 1, y - 1)) + Map[x][y]
-        elif x == N - 1:
-            result[x][y] = max(solution(result, x - 1, y - 1), solution(result, x, y - 1)) + Map[x][y]
-        else:
-            result[x][y] = max(solution(result, x - 1, y - 1), solution(result, x, y - 1),
-                               solution(result, x + 1, y - 1)) + Map[x][y]
-    return result[x][y]
+def solution(Map, N, M):
+    DP = [[0]*M for _ in range(N)]
+    for i in range(N):
+        DP[i][0] = Map[i][0]
+    for j in range(1, M):
+        for i in range(N):
+            if 0 <= i - 1 < N:
+                DP[i][j] = max(DP[i][j], DP[i-1][j-1] + Map[i][j])
+            if 0 <= i + 1 < N:
+                DP[i][j] = max(DP[i][j], DP[i+1][j-1] + Map[i][j])
+            DP[i][j] = max(DP[i][j], DP[i][j-1] + Map[i][j])
+    Max = 0
+    for i in range(N):
+        Max = max(DP[i][M-1], Max)
+    return Max
 
 
 T = int(input())
-for tc in range(T):
-    N, M = map(int, input().rstrip().split())
-    L = list(map(int, input().rstrip().split()))
-    # print(L)
-    Map = [[-1] * M for _ in range(N)]
-    result = [[-1] * M for _ in range(N)]
-    t = 0
+for _ in range(T):
+    N, M = map(int, input().split())
+    L = list(map(int ,input().split()))
+    Map = [[0]*M for _ in range(N)]
+    index = 0
     for i in range(N):
         for j in range(M):
-            Map[i][j] = L[t]
-            t += 1
-    for i in range(N):
-        solution(result, i, M - 1)
-    max_value = -1
-    for i in range(N):
-        max_value = max(max_value, result[i][M-1] )
-    print(max_value)
+            Map[i][j] = L[index]
+            index += 1
+    print(solution(Map,  N, M))
+
+"""
+예시 인풋
+2
+3 4
+1 3 3 2 2 1 4 1 0 6 4 7
+4 4 
+1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
+"""
