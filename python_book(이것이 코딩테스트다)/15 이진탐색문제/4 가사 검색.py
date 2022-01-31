2964e0dde3079687ec88d63d77d0e6a298218f8a
lee <aksndk123@naver.com>
2022-01-31 17:22:45 +0900

    금방 찾아서 다행인데 ???o 에대해서 aaao~zzzo 에는 aaaz도 들어가기 때문에 word에 대해서 거꾸로인 값을 만들어서 이진탐색 떄려야한다. 그리고 탐색을 할려면 정렬된 상태여야한다. 정렬된 값에 대해서 거꾸로해서 다시 집어 넣는다면 정렬이 꺠지기 때문에 집어 넣은다음에 다시 정렬을 하는식으로 풀자

--- python_book(이것이 코딩테스트다)/15 이진탐색문제/4 가사 검색.py
+++ python_book(이것이 코딩테스트다)/15 이진탐색문제/4 가사 검색.py
@@ -1,20 +1,34 @@
+# from bisect import bisect_left, bisect_right
+#
+# def solution(words, queries):
+#     answer = []
+#     word_by_len = [[] for _ in range(11000)]
+#     reverse_by_len = [[] for _ in range(11000)]
+#     for w in words:
+#         word_by_len[len(w)].append(w)
+#         reverse_by_len[len(w)].append(w[::-1])
+#     for w in range(len(word_by_len)):
+#         word_by_len[w].sort()
+#         reverse_by_len[w].sort()
+#     for q in queries:
+#         len_q = len(q)
+#         # 거꾸로
+#         cnt = 0
+#         if q[0] == '?':
+#             rq = q[::-1]
+#             ra = rq.replace('?', "a")
+#             rz = rq.replace('?', "z")
+#             cnt = bisect_right(reverse_by_len[len_q], rz) - bisect_left(reverse_by_len[len_q], ra)
+#         elif q[0] != '?':
+#             qa = q.replace('?', "a")
+#             qz = q.replace('?', 'z')
+#             cnt = bisect_right(word_by_len[len_q], qz) - bisect_left(word_by_len[len_q], qa)
+#         answer.append(cnt)
+#     return answer
+
 from bisect import bisect_left, bisect_right
 
-def solution(words, queries):
-    answer = []
-    word_by_len = [[] for _ in range(11000)]
-    reverse_by_len = [[] for _ in range(11000)]
-    for w in words:
-        word_by_len[len(w)].append(w)
-        reverse_by_len[len(w)].append(w[::-1])
-    for w in range(len(word_by_len)):
-        word_by_len[w].sort()
-        reverse_by_len[w].sort()
-    for q in queries:
-        len_q = len(q)
-        # 거꾸로
-        cnt = 0
-        if q[0] == '?':from bisect import bisect_left, bisect_right
+
 def solution(words, queries):
     cnt_words = [[] for _ in range(10001)]
     cnt_reverse_words = [[] for _ in range(10001)]
@@ -43,6 +57,8 @@ def solution(words, queries):
             cnt = bisect_right(cnt_reverse_words[len(querie)], right) - bisect_left(cnt_reverse_words[len(querie)], left)
         res.append(cnt)
     return res
+
+
 words = list(input().split())
 queries = list(input().split())
 print(solution(words,queries))
@@ -62,18 +78,3 @@ fro?? ????o fr??? fro??? pro?
                 right += q
         이부분을 간편하게 left = q.replace("?", "a") 처럼 사용해도 되는데 시간초과 뜨니 상관없음
 """
-            rq = q[::-1]
-            ra = rq.replace('?', "a")
-            rz = rq.replace('?', "z")
-            cnt = bisect_right(reverse_by_len[len_q],rz) - bisect_left(reverse_by_len[len_q], ra)
-        elif q[0] != '?':
-            qa= q.replace('?', "a")
-            qz= q.replace('?', 'z')
-            cnt = bisect_right(word_by_len[len_q], qz) - bisect_left(word_by_len[len_q], qa)
-        answer.append(cnt)
-    return answer
-
-
-words = list(input().split())
-queries = list(input().split())
-print(solution(words,queries))
