26b3d78c3ef8ae1fe52935b250bd1d43de667c04
lee <aksndk123@naver.com>
2022-02-03 19:48:30 +0900

    DP는 안되는 이유에 대해서 한번 생각해보자 깔끔한 코딩으로 풀이한걸로 수정
    DP불가능이유는 한번한건 예를 들어서 DP[1][1]을 구했으면 한번더 이값이 바뀌는 경우는 거의 없어야하는데 이번경우의 경우 경로가 드러운경우 DP[1][1]값이 또 바껴야하는 경우가 생겨서 시간복잡도가 N^N을 넘어버림
    시간복잡도도 그렇고 굳이 다익스트라 놔두고 쓸필요가 없음
    효율적으로 그렇다는거임

--- python_book(이것이 코딩테스트다)/17 최단 경로 문제/3 화성탐사.py
+++ python_book(이것이 코딩테스트다)/17 최단 경로 문제/3 화성탐사.py
@@ -1,35 +1,28 @@
 import heapq
-INF = int(1e9)
-T = int(input())
-steps = [(1,0), (-1,0),(0,1),(0,-1)]
-for _ in range(T):
-    N = int(input())
-    Map = []
-    res = []
-    res = [[INF]*N for c in range(N)]
-    for i in range(N):
-        Map.append(list(map(int, input().split())))
+
+
+def solution(L, N):
+    steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    INF = int(1e10)
+    dij = [[INF]*N for _ in range(N)]
     q = []
-    res[0][0] = Map[0][0]
-    heapq.heappush(q,(res[0][0],0,0))
+    dij[0][0] = L[0][0]
+    heapq.heappush(q, (L[0][0], 0, 0))
     while q:
-        value, x, y = heapq.heappop(q)
-        if value > res[x][y]:
-            # print(3)
+        dist, x, y = heapq.heappop(q)
+        if dij[x][y] < dist:
             continue
-        for step in steps:
-            nx = x + step[0]
-            ny = y + step[1]
-            if 0<= nx < N and 0 <= ny < N:
-                # print(1)
-                dist = value + Map[nx][ny]
-                if res[nx][ny] > dist:
-                    # print(2)
-                    res[nx][ny] = dist
-                    heapq.heappush(q,(dist, nx, ny))
-    # for r in res:
-    #     print(r)
-    print(res[N-1][N-1])
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N:
+                if dij[nx][ny] > dist + L[nx][ny]:
+                    dij[nx][ny] = dist + L[nx][ny]
+                    heapq.heappush(q, (dij[nx][ny], nx, ny))
+    return dij[N - 1][N - 1]
 
 
-
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    L = [list(map(int, input().split())) for _ in range(N)]
+    print(solution(L, N))
