ee903cc7588e3fddc6d762ac84e943b77acf69c3
lee <aksndk123@naver.com>
2022-02-04 21:06:39 +0900

    BFS에서 q에 dist값을 넣는 대신 visit을 -1로 초기화해서 visit에 거리를 저장하는 방법
    q에 dist를 넣는대신 visit을 -1로 초기화 하고 현재위치를 0으로 둔다
    그다음에 방문하는거에 대해서 visit[nx][ny] = visit[x][y] + 1로 두면 visit에 dist값을 저장할수 있음

--- python_book(이것이 코딩테스트다)/삼성 2020기출/1. 아기상어.py
+++ python_book(이것이 코딩테스트다)/삼성 2020기출/1. 아기상어.py
@@ -1,65 +1,68 @@
 from collections import deque
-
-steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-
-def sizing(size, now_index):
-    if size == now_index:
-        size += 1
-        now_index = 0
-    return size, now_index
-
-
-#  이동 가능 -> time 까지 계산 + 1리턴 // 못움직이면 0 리턴
-def BFS(L, size, sx, sy):
-    global N, time
-    visit = [[-1]*N for _ in range(N)]
-    visit[sx][sy] = 0
-    q = deque()
-    q.append((sx,sy))
-    while q:
-        x, y = q.popleft()
-        for dx, dy in steps:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N and visit[nx][ny] == -1 and L[nx][ny] <= size:
-                q.append((nx, ny))
-                visit[nx][ny] = visit[x][y] + 1
-    min = int(1e9)
-    min_index = 0
+def cal_distance(now_shark):
+    global N, L, visit, now_size
+    steps = [(-1,0),(0,-1),(1,0),(0,1)]
     for i in range(N):
         for j in range(N):
-            if visit[i][j] > 0 and 0 < L[i][j] < size:
-                if visit[i][j] < min:
-                    min = visit[i][j]
-                    min_index = (i, j)
-    if min_index != 0:
-        time += min
-        x, y = min_index
-        L[sx][sy] = 0
-        L[x][y] = 9
-        return x, y
-    else:
-        return 0
-
+            visit[i][j] = 0
+    x, y = shark_now
+    min_dist = int(1e10)
+    min_list = []
+    visit[x][y] = 1
+    q = deque()
+    q.append((x,y,0))
+    """
+    q에 dist를 넣는대신 visit을 -1로 초기화 하고 현재위치를 0으로 둔다
+    그다음에 방문하는거에 대해서 visit[nx][ny] = visit[x][y] + 1로 두면 visit에 dist값을 저장할수 있음 
+    """
+    while q:
+        x, y, dist = q.popleft()
+        # 처음으로 먹을수 있는 물고기를 찾은경우
+        if L[x][y] < now_size and 1 <= L[x][y] <= 6 and dist < min_dist:
+            min_dist = dist
+            min_list = [(x, y)]
+        # 물고기를 먹는데 같은 거리가 있는 경우
+        elif L[x][y] < now_size and 1 <= L[x][y] <= 6 and dist == min_dist:
+            min_list.append((x, y))
+        # 최소거리보다큰 먹을수 있는 물고기의 경우는 필요없으니 break
+        elif dist > min_dist:
+            break
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N and visit[nx][ny] == 0:
+                if L[nx][ny] <= now_size:
+                    visit[nx][ny] = 1
+                    q.append((nx, ny, dist + 1))
+    return min_list, min_dist
 
 
 N = int(input())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
-# 현재 위치 sx sy 로 두기
+L = [list(map(int, input().split())) for _ in range(N)]
+visit = [[0]*N for _ in range(N)]
+shark_now = (0,0)
+now_size = 2
+now_fish = 0
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 9:
-            sx, sy = i, j
-size = 2
-now_index = 0
+        if L[i][j] == 9:
+            shark_now = (i, j)
+            break
 time = 0
 while 1:
-    a = BFS(graph, size, sx, sy)
-    if a == 0:
+    min_list, min_dist = cal_distance(shark_now)
+    if len(min_list) == 0:
         break
     else:
-        sx, sy = a
-    size, now_index = sizing(size, now_index + 1)
+        min_list.sort()
+        x, y = min_list[0]
+        nx, ny = shark_now
+        L[nx][ny] = 0
+        L[x][y] = 9
+        now_fish += 1
+        if now_fish == now_size:
+            now_size += 1
+            now_fish = 0
+        time += min_dist
+        shark_now = (x, y)
 print(time)
+
