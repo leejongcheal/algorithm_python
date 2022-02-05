2023153b94edbd426e51d637fafb33ba24628fb7
lee <aksndk123@naver.com>
2022-02-05 15:58:38 +0900

    *** 아 0,0 시작에 물고기번호가 1~16까지인데 0,0에 0값이 들어가 있다고 가정하고 시작함 ㅋㅋㅋ 전의 풀이는 [0 0 0 0 0 0 0 0]으로 두어서 2개 씩 나누는 indexing을 사용했는데 귀찮아서 하나를 튜플로 묶어서 풀었음 튜플의 값은 변경이 안되니 새로운 튜플값으로 원소 넣어줘야하는것도 잊지말고

--- python_book(이것이 코딩테스트다)/삼성 2020기출/2. 청소년 상어.py
+++ python_book(이것이 코딩테스트다)/삼성 2020기출/2. 청소년 상어.py
@@ -1,81 +1,64 @@
 from collections import deque
+steps = [(-1, 0),(-1, -1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1)]
+def move_fish(L):
+    #  물고기 번호순 이동
+    for fish_number in range(1, 17):
+        flag = 0
+        for i in range(4):
+            for j in range(4):
+                if L[i][j][0] == fish_number:
+                    x, y = i, j
+                    d = L[i][j][1]
+                    flag = 1
+        if flag == 0:
+            # 물고기번호 없는 경우 무시
+            continue
+        for plusd in range(8):
+            nd = (d + plusd) % 8
+            dx, dy = steps[nd]
+            nx, ny = x + dx, y+ dy
+            if 0 <= nx < 4 and 0 <= ny < 4 and L[nx][ny][0] != -1:
+                L[x][y] = (fish_number, nd)
+                L[x][y], L[nx][ny] = L[nx][ny], L[x][y]
+                break
 
-steps = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
 
 
-def fish_move(L, x, y):
-    d = L[x][y * 2 + 1]
-    for i in range(8):
-        nd = (d + i)%8
-        nx, ny = x + steps[nd][0], y + steps[nd][1]
-        # 이동 가능한경우
-        if 0 <= nx < 4 and 0 <= ny < 4 and L[nx][2 * ny] != -1:
-            d = nd
-            L[x][2 * y], L[nx][ny * 2] = L[nx][ny * 2], L[x][y * 2]
-            L[x][2 * y + 1], L[nx][ny * 2 + 1] = L[nx][ny * 2 + 1], d
-            break
-
-
-def find_index(L, n):
-    for i in range(4):
-        for j in range(4):
-            if L[i][2 * j] == n:
-                return (i, j)
-    return 0
-
-
-# 상어 -1 ++ 값과 방향 둘다 이동
-def fish_all_move(L):
-    for n in range(1, 17):
-        a = find_index(L, n)
-        if a != 0:
-            x, y = a
-            fish_move(L, x, y)
 
 
 def deep_copy(L):
-    C = [[-1] * 8 for _ in range(4)]
+    temp = [[0]*4 for _ in range(4)]
     for i in range(4):
         for j in range(4):
-            C[i][2 * j], C[i][j * 2 + 1] = L[i][2 * j], L[i][2 * j + 1]
-    return C
+            temp[i][j] = L[i][j]
+    return temp
 
-
-def BFS(L, res_list):
-    q = deque()
-    res = L[0][0]
-    L[0][0] = -1
-    sx, sy = 0, 0
-    q.append((L, sx, sy, res))
-    while q:
-        L, sx, sy, res = q.popleft()
-        fish_all_move(L)
-        x, y = sx, sy
-        d = L[x][y*2 +1]
-        flag = 0
-        for i in range(4):
-            nx, ny = x + i*(steps[d][0]), y + i*(steps[d][1])
-            if 0 <= nx < 4 and 0 <= ny < 4:
-                if L[nx][ny*2] > 0:
-                    fish_number = L[nx][ny*2]
-                    C = deep_copy(L)
-                    C[sx][sy*2] = 0
-                    C[nx][ny*2] = -1
-                    q.append((C,nx, ny, res + fish_number))
-                    flag = 1
-            else:
-                break
-        if flag == 0:
-            res_list.append(res)
-
-# 입력및 수정
-graph = []
-for _ in range(4):
-    graph.append(list(map(int, input().split())))
+L = [[0]*4 for _ in range(4)]
 for i in range(4):
-    for j in range(4):
-        graph[i][2 * j + 1] -= 1
-# 상어 -1, 빈공간 0 나머지는 물고기 고유번호
-res_list = []
-BFS(graph, res_list)
-print(max(res_list))
+    a1, d1, a2, d2, a3, d3, a4, d4 = map(int, input().split())
+    d1, d2, d3, d4 = d1 -1 , d2 -1 ,d3 - 1, d4 - 1
+    L[i][0] = (a1, d1)
+    L[i][1] = (a2, d2)
+    L[i][2] = (a3, d3)
+    L[i][3] = (a4, d4)
+res = L[0][0][0]
+x, y = 0, 0
+d = L[0][0][1]
+L[0][0] = (-1, d)
+q = deque()
+q.append((L,x,y,d,res))
+while q:
+    L, x, y, d, total = q.popleft()
+    res = max(res, total)
+    move_fish(L)
+    # 상어 이동 가능한경우 다 q에 넣기
+    for i in range(1, 4):
+        dx, dy = steps[d]
+        nx, ny = x + dx*i, y + dy*i
+        if 0 <= nx < 4 and 0 <= ny < 4 and L[nx][ny][0] > 0:
+            temp = deep_copy(L)
+            temp[x][y] = (0, d)
+            get_fish = temp[nx][ny]
+            temp[nx][ny] = (-1, get_fish[1])
+            q.append((temp,nx,ny,get_fish[1], total + get_fish[0]))
+print(res)
