551df32a8e39e37dd10b19d8a3c6057af6344e28
lee <aksndk123@naver.com>
2022-02-07 11:26:16 +0900

    *** (간결한 코드로 수정)복잡한 자료사용 + 위상정렬의 미숙함 + 스택이 아닌 큐로 풀어야함 -> why? 모르겠음 느낌적으로 큐가 맞음

--- python_book(이것이 코딩테스트다)/10 그래프 알고리즘/10-3 커리큘럼.py
+++ python_book(이것이 코딩테스트다)/10 그래프 알고리즘/10-3 커리큘럼.py
@@ -1,36 +1,27 @@
 from collections import deque
-INF = int(1e10)
 N = int(input())
-indegree = [0]*N
-# 해당 i -> j g[i] = j 꼴
-graph = [[] for _ in range(N)]
-# i -> j 일때 in_index[j] = i 꼴
-in_index = [[] for _ in range(N)]
-cost = [0]*N
-# INF값으로 초기화했는데 그러면... max(res[now], cost + res[pre]) 처리를 못하자나..
-res = [0]*N
-result = 0
-# 아무래도 문제가 있을수 있으니 스택대신 큐를 사용하는듯?
+L = [[] for _ in range(N+1)]
+time = [0]*(N+1)
+indegree = [0]*(N+1)
 q = deque()
-for i in range(N):
-    L = list(map(int, input().split()))
-    cost[i] = L[0]
-    indegree[i] = len(L) - 2
+result = []
+for i in range(1, N + 1):
+    l = list(map(int, input().split()))
+    time[i] = l[0]
+    indegree[i] = len(l) - 2
     if indegree[i] == 0:
         q.append(i)
-        res[i] = cost[i]
-    for j in range(1, len(L)-1):
-        in_index[i].append(L[j] - 1)
-        graph[L[j] - 1].append(i)
+    for j in range(1, len(l) - 1):
+        pre = l[j]
+        L[pre].append(i)
+for i in range(N+1):
+    result.append(time[i])
 while q:
     now = q.popleft()
-    if len(in_index[now])!= 0:
-        for in_ind in in_index[now]:
-            res[now] = max(res[now], res[in_ind] + cost[now])
-    for next in graph[now]:
+    for next in L[now]:
         indegree[next] -= 1
+        result[next] = max(result[next], time[next] + result[now])
         if indegree[next] == 0:
             q.append(next)
-# 사실 grapp[now] 의 값이 없다는것은 끝점임으로 이점에 대해서만 max해서 구할려고함
-# 그러나 어쩌피 제일 오래걸리는점이 끝점임은 자명함 따라서 max(res)로 바꿈
-print(max(res))
+for r in result[1:]:
+    print(r)
