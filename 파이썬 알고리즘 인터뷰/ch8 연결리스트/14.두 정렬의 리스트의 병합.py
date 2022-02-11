f3513c522230a603e2bd2242dd1daa93d1a3380d
lee <aksndk123@naver.com>
2022-02-11 09:46:10 +0900

    * 재귀함수를 이용한 풀이 + 주석으로 내용이해 쉽게
    솔직히 이렇게 절대 못풀듯 나는 이해가 힘들어
    걍 파이썬으로 리스트를 하는거 자체가 문제이긴한데 ㅋㅋ 머리
    쓰기용 인듯

--- 파이썬 알고리즘 인터뷰/ch8 연결리스트/14.두 정렬의 리스트의 병합.py
+++ 파이썬 알고리즘 인터뷰/ch8 연결리스트/14.두 정렬의 리스트의 병합.py
@@ -5,28 +5,22 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        head1, head2 = list1, list2
-        res = ListNode(0)
-        head_r = res
-        while head1 and head2:
-            if head1.val < head2.val:
-                head_r.next = ListNode(head1.val)
-                head1 = head1.next
-            else:
-                head_r.next = ListNode(head2.val)
-                head2 = head2.next
-            head_r = head_r.next
-        while head1:
-            head_r.next = ListNode(head1.val)
-            head1 = head1.next
-            head_r = head_r.next
-        while head2:
-            head_r.next = ListNode(head2.val)
-            head2 = head2.next
-            head_r = head_r.next
-        return res.next
-
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # 종료조건을 일부로줌
+        if not l1 and not l2:
+            return None
+        # l1이 없으면 l2의 값을 주고, 둘다 있으면 l2값이 l1보다 작은경우 l1이 작은값을 가지도록함
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        # l1에 값이 없다면 l2에도 없다는 뜻이니 끝나야함
+        # l1에 값이 있다면 더 돌아야함
+        if l1:
+            # l1.next에는 l1.next or l2중 작은값이 들어감
+            # 실제 코드에서는 뒤에 연결리스트까지 정렬된 상태의 연결리스트가 들어감
+            # ex) l1 1->2->3 일때 l1.next 에 1->2->3->4->4 가 들어가서
+            # l1 1->   1->2->3->4->4 가 됨
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
 
 l1 = ListNode(1)
 l1.next= ListNode(2)
