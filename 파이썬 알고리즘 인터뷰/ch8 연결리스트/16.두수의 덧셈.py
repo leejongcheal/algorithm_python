2cac57167a2d7c078a65991d65f1050d32f71d0a
lee <aksndk123@naver.com>
2022-02-11 11:31:02 +0900

    1.일단 그냥 탐색후 문자열로 받아서 계산하는 단순한 방법  - 이경우 반대로하는 연산을 여러번 해야함 2. carry 와 divmod를 이용한 연산
    243 + 564 = 804 가 나오는데 일반적인 수계산이 아닌 앞에서부터 더해가는 연산으로 생각하면 편함
    2+7 = 7, 4 + 6 = 0 carry 1,3 + 4 + carry = 8
    708 넣어주면됨 -> 이걸이해했으면 우아한 풀이가능

---
+++ 파이썬 알고리즘 인터뷰/ch8 연결리스트/16.두수의 덧셈.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = head = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        return root.next
+
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+a = Solution().addTwoNumbers(l1,l2)
+while a:
+    print(a.val, "->",end ="")
+    a = a.next
