5e7d3cf78ac010636528282a855594a401c72049
lee <aksndk123@naver.com>
2022-02-11 14:31:47 +0900

    인덱스로 홀짝 따지는 문제이고, 중요한것은 연결을 할때 전의값과 다음의 값에 대해서 생각하면서 리스트관련 코드를 짜야함 -> 파이썬의 개같은 특징때문에

---
+++ 파이썬 알고리즘 인터뷰/ch8 연결리스트/18.홀짝 연결 리스트.py
@@ -0,0 +1,28 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+        odd = head
+        even = head.next
+        even_head = even
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+        odd.next = even_head
+        return head
+
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(3)
+l1.next.next.next = ListNode(4)
+l1.next.next.next.next = ListNode(5)
+a = Solution().oddEvenList(l1)
+while a:
+    print(a.val, "->",end ="")
+    a = a.next
