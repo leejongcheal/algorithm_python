c5a1c9957812bdbd389a17e0f46f8ca098b405e5
lee <aksndk123@naver.com>
2022-02-12 20:07:48 +0900

    dfs 코드 수정
    def 안에 def 선언할시 self.dfs 이런식으로 불러낼 필요가 없어지는것만 확인
    뺴서 쓰는 경우엔 self.dfs이런식으로 사용해야함
    grid 자체는 그냥 Solution 내에서 찾아서 쓰는 듯 dfs를 위로 선언하든 밑으로 선언하든 상관없이 쓸수 있음 -> 깔끔을 위해서 안쪽에 선언하자

--- 파이썬 알고리즘 인터뷰/ch12 그래프/32.섬의 갯수.py
+++ 파이썬 알고리즘 인터뷰/ch12 그래프/32.섬의 갯수.py
@@ -4,27 +4,24 @@ from collections import deque
 
 
 class Solution:
-    def dfs(self, grid: List[List[str]], x:int, y:int) -> None:
-        steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        for dx, dy in steps:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
-                if grid[nx][ny] == "1":
-                    grid[nx][ny] = '0'
-                    self.dfs(grid, nx, ny)
-
     def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x: int, y: int) -> None:
+            steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for dx, dy in steps:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                    if grid[nx][ny] == "1":
+                        grid[nx][ny] = '0'
+                        dfs(nx, ny)
         N = len(grid)
         M = len(grid[0])
         index = 0
         for i in range(N):
             for j in range(M):
                 if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
+                    dfs(i, j)
                     index += 1
         return index
-
-
 # grid = [
 #   ["1","1","1","1","0"],
 #   ["1","1","0","1","0"],
