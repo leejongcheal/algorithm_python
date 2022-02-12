723f0b9b1165f9e44ca2f0d134fd53be5ad7b8b6
lee <aksndk123@naver.com>
2022-02-12 14:59:40 +0900

    그냥 무식하게 dic 이용해서 해싱 없이 구현

--- 파이썬 알고리즘 인터뷰/ch11 해시 테이블/28.해시맵 디자인.py
+++ 파이썬 알고리즘 인터뷰/ch11 해시 테이블/28.해시맵 디자인.py
@@ -11,13 +11,24 @@ class MyHashMap:
         if self.dic.get(key, 0) != 0:
             del self.dic[key]
 
+
 myHashMap = MyHashMap();
 print(myHashMap.put(1, 1))#; // The map is now [[1,1]]
 print(myHashMap.put(2, 2))#; // The map is now [[1,1], [2,2]]
 print(myHashMap.get(1))#;    // return 1, The map is now [[1,1], [2,2]]
-print(myHashMap.get(3))#;    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+print(myHashMap.get(2))#;    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
 print(myHashMap.put(2, 1))#; // The map is now [[1,1], [2,1]] (i.e., update the existing value)
 print(myHashMap.get(2))#;    // return 1, The map is now [[1,1], [2,1]]
 print(myHashMap.remove(2))#; // remove the mapping for 2, The map is now [[1,1]]
 print(myHashMap.remove(2))#; // remove the mapping for 2, The map is now [[1,1]]
 print(myHashMap.get(2))#;    // return -1 (i.e., not found), The map is now [[1,1]]
+# myHashMap.put(3,11)
+# myHashMap.put(4, 13)
+# myHashMap.put(15, 6)
+# myHashMap.put(6, 15)
+# myHashMap.put(8,8)
+# myHashMap.put(11,0)
+# print(myHashMap.table[11].value)
+# print(myHashMap.get(11))
+# myHashMap.put(1,10)
+# myHashMap.put(12,14)
