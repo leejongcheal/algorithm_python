eb6361a444ef904e213aa08bcd1bb0b312152bcd
lee <aksndk123@naver.com>
2022-02-12 19:54:02 +0900

    굳이 dfs풀이  bfs:280 dfs:480 시간차이

--- 파이썬 알고리즘 인터뷰/ch12 그래프/32.섬의 갯수.py
+++ 파이썬 알고리즘 인터뷰/ch12 그래프/32.섬의 갯수.py
@@ -4,25 +4,23 @@ from collections import deque
 
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def dfs(self, grid: List[List[str]], x:int, y:int) -> None:
         steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                if grid[nx][ny] == "1":
+                    grid[nx][ny] = '0'
+                    self.dfs(grid, nx, ny)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
         N = len(grid)
         M = len(grid[0])
-        q = deque()
         index = 0
         for i in range(N):
             for j in range(M):
                 if grid[i][j] == '1':
-                    q.append((i, j))
-                    grid[i][j] = '0'
-                    while q:
-                        x, y = q.popleft()
-                        for dx, dy in steps:
-                            nx, ny = x + dx, y + dy
-                            if 0 <= nx < N and 0 <= ny < M:
-                                if grid[nx][ny] == '1':
-                                    grid[nx][ny] = '0'
-                                    q.append((nx, ny))
+                    self.dfs(grid, i, j)
                     index += 1
         return index
 
