7c14e508099f428b247571c2cccb89323f19c791
lee <aksndk123@naver.com>
2022-02-13 21:44:50 +0900

    ** 솔직히 전의 풀이에서 이풀이 생각하라 해도 못풀겠다.
    일단 스택과 seen(검색용)있을때 스택에 이미 올라간 경우 해당 char은 -1만하고 탐색은 안하고
    해당 char 이 스택의 위치가 나올때까지 스택을 pop해주는데
    그떄 위치를 찾는 방법으로는 스택의 요소가 char보다 큰 값이고 그뒤에 값이 더있는경우 -> 뒤로 넣을수 있으니깐 이경우에는 stack과 seen에서 다 뺴버린다.
    그러면서 stack 반환하는데 어렵네.

--- 파이썬 알고리즘 인터뷰/ch9 스택, 큐/21.중복 문자 제거.py
+++ 파이썬 알고리즘 인터뷰/ch9 스택, 큐/21.중복 문자 제거.py
@@ -1,11 +1,16 @@
-from collections import  Counter
+from collections import Counter
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):]
-            if set(s) == set(suffix):
-                return char + self.removeDuplicateLetters(suffix.replace(char,""))
-        return ""
+        counter, seen, stack = Counter(s), set(), []
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
+        return "".join(stack)
 
 
 print(Solution().removeDuplicateLetters("cbacdcbc"))
