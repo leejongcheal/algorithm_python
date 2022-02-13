06192f24be6e17723776feac0eeef50a202d2ed4
lee <aksndk123@naver.com>
2022-02-13 20:10:45 +0900

    * 문제이해 못했고 이 발상도 못함

---
+++ 파이썬 알고리즘 인터뷰/ch9 스택, 큐/21.중복 문자 제거.py
@@ -0,0 +1,11 @@
+from collections import  Counter
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char,""))
+        return ""
+
+
+print(Solution().removeDuplicateLetters("cbacdcbc"))
