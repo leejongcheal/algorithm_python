8b1f414f7384af457b49eb61f2f47e9ee96e060e
lee <aksndk123@naver.com>
2022-02-13 16:48:44 +0900

    odd, even = head, head.next로 두고 푸는 풀이

--- 파이썬 알고리즘 인터뷰/ch8 연결리스트/18.홀짝 연결 리스트.py
+++ 파이썬 알고리즘 인터뷰/ch8 연결리스트/18.홀짝 연결 리스트.py
@@ -6,30 +6,21 @@ class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
         if head is None:
             return None
-        odd_head, even_head = ListNode(), ListNode()
-        odd, even = odd_head, even_head
-        index = 1
-        while head:
-            if index%2 == 0:
-                even.next = head
-                even = even.next
-            else:
-                odd.next = head
-                odd = odd.next
-            head = head.next
-            index += 1
-        # if odd.next is not None:
-        #     odd.next = None
-        if even.next is not None:
-            even.next = None
-        odd.next = even_head.next
-        return odd_head.next
+        odd, even = head, head.next
+        even_head = even
+        while even and even.next:
+            odd.next = even.next
+            even.next = even.next.next
+            odd, even = odd.next, even.next
+        odd.next = even_head
+        return head
+
 
 l1 = ListNode(1)
 l1.next = ListNode(2)
 l1.next.next = ListNode(3)
 l1.next.next.next = ListNode(4)
-# l1.next.next.next.next = ListNode(5)
+l1.next.next.next.next = ListNode(5)
 a = Solution().oddEvenList(l1)
 while a:
     print(a.val, "->",end ="")
