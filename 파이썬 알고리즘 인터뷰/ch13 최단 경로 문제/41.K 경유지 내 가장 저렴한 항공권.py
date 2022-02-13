45ce36342a7ec57ecbaf9120b4e98d0b26191481
lee <aksndk123@naver.com>
2022-02-13 15:40:42 +0900

    ** if now == dst일때 리턴해도 답이나오는데 왜인지 생각할것 -> 해도 타임오버

--- 파이썬 알고리즘 인터뷰/ch13 최단 경로 문제/41.K 경유지 내 가장 저렴한 항공권.py
+++ 파이썬 알고리즘 인터뷰/ch13 최단 경로 문제/41.K 경유지 내 가장 저렴한 항공권.py
@@ -12,6 +12,10 @@ class Solution:
         q = [(0, src, 0)]
         while q:
             now_dist, now, k = heapq.heappop(q)
+            # 결국엔 dst를 제일 먼저 오는것은 k내의 제일 작은 dist를 가진값이므로 이런식으로 리턴 해도 정답임
+            if now == dst:
+                return now_dist
+            # 현재 k는 다음에 방문할 노드의 현재 경유지 갯수를 뜻함 -> 따라서 조건문 전에 dst이면 상관없음
             if k > K:  # res[now] < now_dist일 경우도 돌아야함 k 기준으로 탐색해야하니
                 continue
             for next, dist in graph[now]:
@@ -21,8 +25,5 @@ class Solution:
                 # 안쪽일 경우에만 값이 제일 작고 k내일때 res 수정
                 # 검사는 k의 기준에 따라서 작은값을 넣어주는식으로 탐색
                 heapq.heappush(q,(next_dist, next, k + 1))
-        if res[dst] >= INF:
-            return -1
-        else:
-            return res[dst]
+        return -1
 print(Solution().findCheapestPrice(5, [[0,1,5],[1,2,5],[0,3,2],[3,1,2],[1,4,1],[4,2,1]],0,2,2))
