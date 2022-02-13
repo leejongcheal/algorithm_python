443e8844ebf8eecd7720776280311dc27d29b875
lee <aksndk123@naver.com>
2022-02-13 16:09:16 +0900

    첫번째 풀이의 경우 쓸때없이 ListNode를 만들어서 넣어줬는데 그럴필요없이 이풀이처럼 풀이하면됨
    솔직히 재귀풀이는 에바임

--- 파이썬 알고리즘 인터뷰/ch8 연결리스트/14.두 정렬의 리스트의 병합.py
+++ 파이썬 알고리즘 인터뷰/ch8 연결리스트/14.두 정렬의 리스트의 병합.py
@@ -6,21 +6,28 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        # 종료조건을 일부로줌
-        if not l1 and not l2:
-            return None
-        # l1이 없으면 l2의 값을 주고, 둘다 있으면 l2값이 l1보다 작은경우 l1이 작은값을 가지도록함
-        if (not l1) or (l2 and l1.val > l2.val):
-            l1, l2 = l2, l1
-        # l1에 값이 없다면 l2에도 없다는 뜻이니 끝나야함
-        # l1에 값이 있다면 더 돌아야함
-        if l1:
-            # l1.next에는 l1.next or l2중 작은값이 들어감
-            # 실제 코드에서는 뒤에 연결리스트까지 정렬된 상태의 연결리스트가 들어감
-            # ex) l1 1->2->3 일때 l1.next 에 1->2->3->4->4 가 들어가서
-            # l1 1->   1->2->3->4->4 가 됨
-            l1.next = self.mergeTwoLists(l1.next, l2)
-        return l1
+        root = now = ListNode()
+        head1, head2 = l1, l2
+        while head1 is not None and head2 is not None:
+            if head1.val <= head2.val:
+                now.next = head1
+                head1 = head1.next
+            else:
+                now.next = head2
+                head2 = head2.next
+            now = now.next
+        while head1 is not None:
+            now.next = head1
+            head1 = head1.next
+            now = now.next
+        while head2 is not None:
+            now.next = head2
+            head2 = head2.next
+            now = now.next
+        return root.next
+
+
+
 
 l1 = ListNode(1)
 l1.next= ListNode(2)
