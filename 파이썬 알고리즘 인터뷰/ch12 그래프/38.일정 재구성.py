d3038a4091372cbe43d3db8e64c7986fadd72744
lee <aksndk123@naver.com>
2022-02-14 22:39:17 +0900

    ** 내가 이해할수 있는 dfs 백트래킹으로 풀이
    하나의 경로에 대해서 visit 처리를 위해서 graph에서 그값을 뻈다가 검사후에 append + sort로 넣어주는식으로 처리함

--- 파이썬 알고리즘 인터뷰/ch12 그래프/38.일정 재구성.py
+++ 파이썬 알고리즘 인터뷰/ch12 그래프/38.일정 재구성.py
@@ -2,22 +2,26 @@ from typing import List
 from collections import defaultdict
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def dfs(path):
+            now = path[-1]
+            if len(path) == N:
+                return path[:]
+            temp = graph[now][:]
+            for next in temp:
+                graph[now].remove(next)
+                a = dfs(path + [next])
+                if a is not None:
+                    return a
+                graph[now].append(next)
+                graph[now].sort()
+            return None
+        tickets.sort()
         graph = defaultdict(list)
-        # 그래프 순서대로 구성
-        for a, b in sorted(tickets, reverse=1):
+        N = len(tickets) + 1
+        for a, b in tickets:
             graph[a].append(b)
+        return dfs(["JFK"])
 
-        route = []
-        def dfs(a):
-            # 첫 번째 값을 읽어 어휘순 방문
-            while graph[a]:
-                # pop 연산을 통해서 재방문하는 경우도 제한함
-                dfs(graph[a].pop())
-            route.append(a)
-
-        dfs('JFK')
-        # 다시 뒤집어 어휘순 결과로
-        return route[::-1]
 
 
 print(Solution().findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
