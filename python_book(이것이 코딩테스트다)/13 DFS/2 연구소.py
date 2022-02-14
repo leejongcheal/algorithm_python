9ea614ae09ec3f028d4939ebbff9a68049736633
lee <aksndk123@naver.com>
2022-02-14 16:17:30 +0900

    *** dfs풀이 + 벽을 치는경우의수를 줄이는 테크닉
    prev_index을 보면 x,y를 벽으로 친경우 다음 벽은 x,y보다 큰값을 넣어줘서 방문을 줄임 -> 이렇게 해도 반복되는 경우가 생기는줄 알았음
    -> 이를 방지하기위해set_list를 썼음 -> 그러나 이문제에선 뻘짓임
    
    + set_list에서 (a,b,c)를 원소로 가지는데 (b,a,c)도 똑같은걸로 보기에 원래하면 tuple(sorted(prev_index))로 해서 넣어줘야함 -> 그러나 여기서는 순서대로 들어가는게 지켜지기에 필요없음
    
     but, 전의 벽보다 큰 값만 벽을 넣어주는식으로 해도 반복되는 경우의 수가 없으므로 visit_list를 쓸필요가 없음

--- python_book(이것이 코딩테스트다)/13 DFS/2 연구소.py
+++ python_book(이것이 코딩테스트다)/13 DFS/2 연구소.py
@@ -1,78 +1,56 @@
-from collections import deque
-
-
-def comb(L, M):
-    N = len(L)
-    if N < M:
-        return None
-    res = []
-    if M == 1:
-        for l in L:
-            res.append([l])
-    else:
-        for i in range(N - M + 1):
-            for temp in comb(L[i + 1:], M - 1):
-                res.append([L[i]] + temp)
-    return res
-
-
-# visit을 안써도 0인 부분에 대해서 검사를  하기떄문에 상관없을듯? 크기도 작고
-# 확인필요 -> 안해도 상관없음
-def BFS(Map, x, y):
-    q = deque()
-    q.append((x, y))
-    global steps, N, M
-    while q:
-        x, y = q.popleft()
-        for dx, dy in steps:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < M:
-                if Map[nx][ny] == 0:
-                    q.append((nx, ny))
-                    Map[nx][ny] = 2
-
-
-def check(Map):
-    global N, M
-    cnt = 0
+from typing import List
+from copy import deepcopy
+def count(L: List):
+    sum = 0
     for i in range(N):
         for j in range(M):
-            if Map[i][j] == 0:
-                cnt += 1
-    return cnt
+            if L[i][j] == 0:
+                sum += 1
+    return sum
 
-
-def copy_list(Map):
-    global N, M
-    # copy_Map을 굳이 계속 새로만들 필요없이 한번만 초기화해주고 Map값을 넣어줘서 사용
-    # 하면 메모리 아낄수 있음
-    # copy_list(Map, copy_Map)으로 두고 메인에서 한번만 copy_Map 초기화
-    copy_Map = [[0] * M for _ in range(N)]
-    for i in range(N):
+def fill_dfs(L:List, x, y):
+    steps = [(1,0),(-1,0),(0,-1),(0,1)]
+    for dx, dy in steps:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < M and L[nx][ny] == 0:
+            L[nx][ny] = 2
+            fill_dfs(L, nx, ny)
+def dfs_3(x, y, k, prev_index: List = []):
+    global res
+    if k == 3:
+        # 일단 이 visit을 안쓴경우에 더 빨랐는데 말이안됨 사실
+        # -> 전의 벽보다 큰값만 벽치게해도 3개든 4개든 겹치는 경우 없어짐
+        # prev_index에 대해서 정렬할 필요없이 알아서 순서대로 들어가게 코딩함
+        if tuple(prev_index) not in set_visit:
+            copy = deepcopy(L)
+            for vx, vy in virus:
+                fill_dfs(copy, vx, vy)
+            # 이런식으로 정렬할 필요가 없음
+            # set_visit.add(tuple(sorted(prev_index)))
+            set_visit.add(tuple(prev_index))
+            res = max(res, count(copy))
+        else:
+            # 확인 결과 상관없음 + 어쩌피 출력안하니 놓아도됨 ㅋㅋ
+            print("3개의벽이 중복되는 경우에 출력 -> 백준결과 출력안함")
+        return
+    for i in range(x, N):
         for j in range(M):
-            copy_Map[i][j] = Map[i][j]
-    return copy_Map
+            if i == x and j < y:
+                continue
+            if L[i][j] == 0:
+                L[i][j] = 1
+                dfs_3(i, j, k + 1, prev_index + [(i, j)])
+                L[i][j] = 0
 
-
-steps = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 N, M = map(int, input().split())
-Map = [list(map(int, input().split())) for _ in range(N)]
+L = [list(map(int, input().split())) for _ in range(N)]
 virus = []
-blank = []
-res = 0
 for i in range(N):
     for j in range(M):
-        if Map[i][j] == 2:
-            virus.append([i, j])
-        elif Map[i][j] == 0:
-            blank.append([i, j])
-for blank_3 in comb(blank, 3):
-    copy_Map = copy_list(Map)
-    for b in blank_3:
-        x, y = b[0], b[1]
-        copy_Map[x][y] = 1
-    for v in virus:
-        x, y = v[0], v[1]
-        BFS(copy_Map, x, y)
-    res = max(res, check(copy_Map))
+        if L[i][j] == 2:
+            virus.append((i, j))
+set_visit = set()
+res = 0
+dfs_3(0,0,0)
 print(res)
+
