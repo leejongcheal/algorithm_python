07a4e9e6f7697cb6a86cfba8625a4031712e2fa9
lee <aksndk123@naver.com>
2022-02-15 01:01:09 +0900

    heapq.heapify(nums) : nums리스트를 힙에 대응되는 구조로 정렬
    후에 값을 추가하거나 삭제할 경우 힙의 구조를 유지하지는 않음

--- 파이썬 알고리즘 인터뷰/ch15 힙/55.배열의 k번째 큰 요소.py
+++ 파이썬 알고리즘 인터뷰/ch15 힙/55.배열의 k번째 큰 요소.py
@@ -4,12 +4,11 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for n in nums:
-            heapq.heappush(heap,-n)
-        for i in range(k):
-            res = heapq.heappop(heap)
-        return -res
+        heapq.heapify(nums)
+        for i in range(len(nums) - k):
+            heapq.heappop(nums)
+        return heapq.heappop(nums)
+
 
 
 print(Solution().findKthLargest([3,2,3,1,2,4,5,5,6], 4))
