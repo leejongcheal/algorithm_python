a03b09660de2900298704a7e173f696d6004da0c
lee <aksndk123@naver.com>
2022-02-14 21:21:21 +0900

    * for문에서 더한값이 target보다 크다면 break로 종료 -> 시간적 효율
    전의꺼랑 차이를 느낄것 + 따라서 정렬이 필요함

--- 파이썬 알고리즘 인터뷰/ch12 그래프/36.조합의 합.py
+++ 파이썬 알고리즘 인터뷰/ch12 그래프/36.조합의 합.py
@@ -1,17 +1,24 @@
 from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(csum, index, path):
-            if csum == 0:
-                res.append(path)
-                return
-            elif csum < 0:
-                return
-            else:
-                for i in range(index,len(candidates)):
-                    dfs(csum - candidates[i],i,path + [candidates[i]])
+        # 전의 풀이는 정렬을 안해도 됨 -> 211111111 같은 값이 들어가기 때문에
+        # 근데 이풀이는 for문에서 now_som + L[next]값이 target보다 크다면
+        # for문에대해서 break 쳐서 다음을 검사안함 -> 시간적으로 좋음
+        # 따라서 정렬을 해서 시간효율을 높인것 - 차이점을 알고 써야함
+        candidates = sorted(candidates)
         res = []
-        dfs(target, 0, [])
+        def dfs(index, now = []):
+            now_sum = sum(now)
+            if now_sum == target:
+                res.append(now)
+                return
+            for i in range(index, len(candidates)):
+                num = candidates[i]
+                if now_sum + num <= target:
+                    dfs(i, now + [num])
+                else:
+                    break
+        dfs(0)
         return res
 
 print(Solution().combinationSum([2,7,6,3,5,1], 9))
