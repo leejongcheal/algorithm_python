9117793e55d8bb16732c89ed7684de885758a303
lee <aksndk123@naver.com>
2022-02-16 15:50:43 +0900

    BST 개념이해및 응용구현

---
+++ 파이썬 알고리즘 인터뷰/ch14 트리/50.정렬된 배열의 이진 탐색 트리 변환.py
@@ -0,0 +1,20 @@
+from typing import List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> [TreeNode]:
+        if len(nums) == 0:
+            return None
+        mid = len(nums) // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+        return node
+
+
+L = [1,2,3,4]
+a = Solution().sortedArrayToBST(L)
+print(1)
