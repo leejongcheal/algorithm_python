d268f1e4597aa94e97e04c88a69a3e757ab206d0
lee <aksndk123@naver.com>
2022-02-17 13:06:07 +0900

    트라이 구현
    apple 의 경우 e에만 True를 줘서 끝나는걸 알려줘야함
    그래서 찾는경우에 모든 word에 대해서 찾았을 경우 마지막값에 True까지 들어가야 찾았다고봄
    apple . appleiij 를 넣었을때
    a f 식이다.
    p f
    p f
    l f
    e t
    i f
    i f
    j t

---
+++ 파이썬 알고리즘 인터뷰/ch16 트라이/56.트라이 구현.py
@@ -0,0 +1,44 @@
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = dict()
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+trie = Trie()
+print(trie.insert("apple"))
+print(trie.search("apple"))
+print(trie.search("app"))
+print(trie.startsWith("app"))
+print(trie.insert("app"))
+print(trie.search("app"))
+print(1)
