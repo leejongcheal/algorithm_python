cbd4579734eecde8275ab16e852f12d472368963
lee <aksndk123@naver.com>
2022-02-17 15:39:18 +0900

    푸는 과정 배우기
    일단result 값을 집어넣은 다음 전의 start보단 다음의 start가 정렬했기에 크다는것을 알고 있으니 reulst의 마지막원소의 start가 다음의 last보다 작거나 같다면 start~ max(prev_last, now_last) 이다.
    이를 위해서 result[-1][1] = max(result[-1][1], now_last)를 통해서
    start값은 고정이고 마지막 끝나는 부분만 수정하는식으로 구성함

---
+++ 파이썬 알고리즘 인터뷰/ch17 정렬/59.구간 병합.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        result = []
+        for start, last in sorted(intervals):
+            if result and start <= result[-1][1]:
+                result[-1][1] = max(result[-1][1], last)
+            else:
+                result.append([start, last])
+        return result
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+# intervals = [[1,3]]
+print(Solution().merge(intervals))
