7c8c6422847fcd59746e15673d2fffa6b6735ea4
lee <aksndk123@naver.com>
2022-02-18 11:41:21 +0900

    any함수를 이용한 파이썬 다운 코딩

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
@@ -2,12 +2,7 @@ from typing import List
 from bisect import bisect_left
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        temp = []
-        for row in matrix:
-            index = bisect_left(row,target)
-            if index < len(row) and row[index] == target:
-                return True
-        return False
+        return any(target in row for row in matrix)
 
 L  = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
-print(Solution().searchMatrix(L,0))
+print(Solution().searchMatrix(L,5))
