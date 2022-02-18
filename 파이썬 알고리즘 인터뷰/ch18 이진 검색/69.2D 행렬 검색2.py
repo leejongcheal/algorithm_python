7ab209e4c6036ff00faea1d31df5636d7eefbeb9
lee <aksndk123@naver.com>
2022-02-18 11:48:02 +0900

    ** 2차원 이진탐색 응용 와우

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
@@ -2,7 +2,14 @@ from typing import List
 from bisect import bisect_left
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        return any(target in row for row in matrix)
-
+        row, col = 0, len(matrix[0]) - 1
+        while row < len(matrix) and col >= 0:
+            if target < matrix[row][col]:
+                col -= 1
+            elif target > matrix[row][col]:
+                row += 1
+            else:
+                return True
+        return False
 L  = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
 print(Solution().searchMatrix(L,5))
