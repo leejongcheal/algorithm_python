9aa6d635d99c43e0b2eb8632fcdaf61b7ffea0c2
lee <aksndk123@naver.com>
2022-02-18 11:38:49 +0900

    귀찮아서 일열로 만들어서 정렬후 이진탐색

---
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
@@ -0,0 +1,15 @@
+from typing import List
+from bisect import bisect_left
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        temp = []
+        for row in matrix:
+            temp += row
+        temp.sort()
+        index = bisect_left(temp,target)
+        if index < len(temp) and temp[index] == target:
+            return True
+        return False
+
+L  = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+print(Solution().searchMatrix(L,5))
