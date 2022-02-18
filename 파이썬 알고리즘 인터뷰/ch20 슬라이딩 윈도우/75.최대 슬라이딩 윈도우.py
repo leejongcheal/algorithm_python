5d8bdc79e6c9d0a4743a19e1fdaed421fe26e65c
lee <aksndk123@naver.com>
2022-02-18 16:04:48 +0900

    * 굳이 복잡한 슬라이딩 쓰지말고 큐에 넣어서 사용하는 방식 --깔끔한 코딩 연습 필요 그러나 시간초과

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/75.최대 슬라이딩 윈도우.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/75.최대 슬라이딩 윈도우.py
@@ -1,20 +1,22 @@
 from typing import List
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        now_max = -int(1e10)
+        INF = int(1e10)
+        now_max = -INF
+        window = deque()
         res = []
-        left, right = 0, k - 1
-        for _ in range(len(nums) - k + 1):
-            # 계속 max해서 구하기보단 최근에 빠진값이 now_max였다면 해당 구간의 최댓값을 구하는식으로 시간효율
-            # left가 0 인경우에도 nums[-1] 마지값은 어쩄든 최솟값보다 크니깐 인덱스범위 오류없이 잘들어감
-            # 의도된 값은 아니니 틀렸다고 보겠음, 그렇다고 left > 0 을 넣어주면 처음에 구간최댓값구하는걸 못함
-            if now_max <= nums[left - 1]:
-                now_max = max(nums[left:right+1])
+        for index, val in enumerate(nums):
+            window.append(val)
+            if index < k - 1:
+                continue
+            if now_max == -INF:
+                now_max = max(window)
             else:
-                now_max = max(nums[right],now_max)
-            left += 1
-            right += 1
+                now_max = max(now_max, val)
             res.append(now_max)
+            if now_max == window.popleft():
+                now_max = -INF
         return res
 
 L = [1,3,-1,-3,5,3,6,7]
