3d78ebf94c80ff3f94105887b73b1c71a862aa2f
lee <aksndk123@naver.com>
2022-02-18 11:15:55 +0900

    굳이 슬라이싱 최소화 아주 조금 빨라짐
    이런식으로 풀수도 있는걸 확인하자

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/68.두수의 합2.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/68.두수의 합2.py
@@ -3,10 +3,11 @@ from bisect import bisect_left, bisect_right
 # 답은 무조건 하나 + L은 정렬된 값이 들어옴
 class Solution:
     def twoSum(self, L: List[int], target: int) -> List[int]:
-        for start in range(len(L) - 1):
-            now_target = target - L[start]
-            index = bisect_left(L[start+1:],now_target)
-            if index + start + 1< len(L) and L[index + start + 1] == now_target:
-                return (start + 1, index + start + 2)
-L = [2,7,11,15]
-print(Solution().twoSum(L,9))
+        for start, val in enumerate(L):
+            now_target = target - val
+            temp = L[start+1:]
+            index = bisect_left(temp, now_target)
+            if index < len(temp) and L[index + start + 1] == now_target:
+                return start + 1, start + index + 2
+L = [5, 25, 75]
+print(Solution().twoSum(L,100))
