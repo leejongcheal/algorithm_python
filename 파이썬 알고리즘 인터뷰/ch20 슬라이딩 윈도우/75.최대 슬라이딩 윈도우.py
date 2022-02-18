bd99d691135d51538796ae1ae96ebd5093ae8951
lee <aksndk123@naver.com>
2022-02-18 16:00:53 +0900

    전의 문제 인덱스 범위 오류

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/75.최대 슬라이딩 윈도우.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/75.최대 슬라이딩 윈도우.py
@@ -6,6 +6,8 @@ class Solution:
         left, right = 0, k - 1
         for _ in range(len(nums) - k + 1):
             # 계속 max해서 구하기보단 최근에 빠진값이 now_max였다면 해당 구간의 최댓값을 구하는식으로 시간효율
+            # left가 0 인경우에도 nums[-1] 마지값은 어쩄든 최솟값보다 크니깐 인덱스범위 오류없이 잘들어감
+            # 의도된 값은 아니니 틀렸다고 보겠음, 그렇다고 left > 0 을 넣어주면 처음에 구간최댓값구하는걸 못함
             if now_max <= nums[left - 1]:
                 now_max = max(nums[left:right+1])
             else:
@@ -16,4 +18,4 @@ class Solution:
         return res
 
 L = [1,3,-1,-3,5,3,6,7]
-print(Solution().maxSlidingWindow(L,3))
+print(Solution().maxSlidingWindow([1,2],1))
