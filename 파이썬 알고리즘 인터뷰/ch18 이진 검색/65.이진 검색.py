a6f642848e91ada4dfc8ba791f931c5a3bde519f
lee <aksndk123@naver.com>
2022-02-18 09:43:38 +0900

    이진검색 구현 + 재귀문의 탈출조건 생각

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/65.이진 검색.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/65.이진 검색.py
@@ -1,13 +1,21 @@
+from typing import List
 from bisect import bisect_left, bisect_right
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # if bisect_right(nums, target) - bisect_left(nums, target) > 0:
-        #     return bisect_left(nums, target)
-        # else:
-        #     return -1
-        index = bisect_left(nums, target)
-        if index < len(nums) and nums[index] == target:
-            return index
-        else:
-            return -1
+        def binary_search(start, end):
+            if start > end:
+                return -1
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                return binary_search(start, mid - 1)
+            else:
+                return binary_search(mid + 1, end)
+
+        return binary_search(0,len(nums) - 1)
+
+L =  [-1,0,3,3,5,9,12]
+# intervals = [[1,3]]
+print(Solution().search(L,1))
