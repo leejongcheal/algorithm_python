a798c8e038c82ee615e1e4dbb36e8c1b5864164a
lee <aksndk123@naver.com>
2022-02-18 16:22:55 +0900

    매우 훌룡한 deque 이용한 o(n)풀이
    전의 풀이는 윈도우 마다 max를 구하는 풀이에 약간의 가지치기를 해버림
    
    이거의 핵심은 q의 첫번째값은 해당 슬라이딩내에서 가장큰값을 가지는것임
    ++ 그리고 다음값에 대해서 q에 다음값보다 작은값이 있다면 기억할 필요없이 다지워버리는 게 핵심
    예를 들어 [6,2,3,4,5] 이고 다음값이 6인 경우
    -> [2,3,4,5]에 대해서 알필요없이 [6]으로 넣어서 더큰값이 안나온다면 해당시간 동안 6의 값을 계속 넣어주면됨

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/75.최대 슬라이딩 윈도우.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/75.최대 슬라이딩 윈도우.py
@@ -1,23 +1,18 @@
-from typing import List
 from collections import deque
+from typing import List
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        INF = int(1e10)
-        now_max = -INF
-        window = deque()
-        res = []
+        q, ans = deque(), []
         for index, val in enumerate(nums):
-            window.append(val)
-            if index < k - 1:
-                continue
-            if now_max == -INF:
-                now_max = max(window)
-            else:
-                now_max = max(now_max, val)
-            res.append(now_max)
-            if now_max == window.popleft():
-                now_max = -INF
-        return res
+            if q and index - q[0][0] == k:
+                q.popleft()
+            while q and q[-1][1] < val:
+                q.pop()
+            q.append((index, val))
+            if index >= k - 1:
+                ans.append(q[0][1])
+        return ans
 
-L = [1,3,-1,-3,5,3,6,7]
-print(Solution().maxSlidingWindow([1,2],1))
+sol = Solution()
+print(sol.maxSlidingWindow(nums=[1,-1], k=1))
