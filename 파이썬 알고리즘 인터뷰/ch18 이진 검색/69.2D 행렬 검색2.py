9c42ca2b609e4729c05c8f2d02740653eb98adbb
lee <aksndk123@naver.com>
2022-02-18 11:40:23 +0900

    그냥 행별로 이진탐색해서 값이 있는지 확인 굳이 복잡한 이진탐색 안해도 됨 -> 시간초과만 안뜨면 되니깐 nlogn풀이

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/69.2D 행렬 검색2.py
@@ -4,12 +4,10 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         temp = []
         for row in matrix:
-            temp += row
-        temp.sort()
-        index = bisect_left(temp,target)
-        if index < len(temp) and temp[index] == target:
-            return True
+            index = bisect_left(row,target)
+            if index < len(row) and row[index] == target:
+                return True
         return False
 
 L  = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
-print(Solution().searchMatrix(L,5))
+print(Solution().searchMatrix(L,0))
