6e7a86a43212fa272dbe73b48a1a0ec109484e17
lee <aksndk123@naver.com>
2022-02-18 11:09:39 +0900

    굳이 이진탐색으로 풀려고 할때, 슬라이싱 잘 확인하자
    ++ 슬라이싱은 속도에 영향을 크게 줌 3초걸림

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/68.두수의 합2.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/68.두수의 합2.py
@@ -1,16 +1,12 @@
 from typing import List
-
+from bisect import bisect_left, bisect_right
 # 답은 무조건 하나 + L은 정렬된 값이 들어옴
 class Solution:
     def twoSum(self, L: List[int], target: int) -> List[int]:
-        left, right = 0, len(L) - 1
-        while left < right:
-            now_sum = L[left] + L[right]
-            if now_sum > target:
-                right -= 1
-            elif now_sum < target:
-                left += 1
-            else:
-                return (left + 1, right + 1)
+        for start in range(len(L) - 1):
+            now_target = target - L[start]
+            index = bisect_left(L[start+1:],now_target)
+            if index + start + 1< len(L) and L[index + start + 1] == now_target:
+                return (start + 1, index + start + 2)
 L = [2,7,11,15]
 print(Solution().twoSum(L,9))
