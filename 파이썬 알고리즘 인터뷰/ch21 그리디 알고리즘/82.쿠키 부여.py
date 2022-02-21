b8946f30407fb59c2e4548b137fca6c75696d1a5
lee <aksndk123@naver.com>
2022-02-21 14:14:10 +0900

    ** 이진 탐색발상 + 코딩
    if index > res:
       res += 1
    **이 부분 이해가 필요함
    G가 학생
    S가 쿠키의 크기
    오른쪽으로 들어갈수 있는곳이 index값인데 만약에 이미 res = 2라면 이미 학생 두명은 받아갔다는 뜻이니 다음 쿠키는 학생3부터 줘야함, 따라서 index가 2라면 학생3의 값보다 작다는 뜻으로 못주는것이다.(이부분 때문에 bisect_right를 사용한다.) 2222로 뭉쳐있는경우 2를 찾으면 index는 4의 값이 나오니 4명까진 res += 1 가능함

--- 파이썬 알고리즘 인터뷰/ch21 그리디 알고리즘/82.쿠키 부여.py
+++ 파이썬 알고리즘 인터뷰/ch21 그리디 알고리즘/82.쿠키 부여.py
@@ -1,17 +1,17 @@
 from typing import List
-
+from bisect import bisect_left, bisect_right
 
 class Solution:
     def findContentChildren(self, G: List[int], S: List[int]) -> int:
-        G.sort(reverse=1)
-        S.sort(reverse=1)
+        G.sort()
+        S.sort()
         res = 0
-        while G and S:
-            s = S.pop()
-            if G[-1] <= s:
-                G.pop()
+        for s in S:
+            index = bisect_right(G,s)
+            if index > res:
                 res += 1
         return res
+
 
 g = [1,2]
 s = [1,2,3]
