ff15628292dbaa7bf2fe905001da61a64a765b5b
lee <aksndk123@naver.com>
2022-02-21 13:11:11 +0900

    *한번만 방문하는 풀이
    솔직히 수학적은 IQ가 있는거 아니면 절대 발상못함 에바문제
    그리고 가스의 합이 비용의 합보다 크다면 무조건 돌수 있다고 푸는데 -> 이부분이 이해가 안감, 반례가 있을꺼 같은데? -> 실제로 해보니 ㅇㅋ
    
    - 다른사람 참고해보니
    애초에 문제를 gas -cost 배열을 만들어서 투포인터로 푸는게 더 직관적인거 같아서 시도해보자

--- 파이썬 알고리즘 인터뷰/ch21 그리디 알고리즘/81.주유소.py
+++ 파이썬 알고리즘 인터뷰/ch21 그리디 알고리즘/81.주유소.py
@@ -5,19 +5,13 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-        for start in range(len(gas)):
-            fuel = 0
-            for i in range(start, len(gas) + start):
-                index = i % len(gas)
-                flag = 1
-                if gas[index] + fuel < cost[index]:
-                    flag = 0
-                    break
-                else:
-                    fuel += gas[index] - cost[index]
-            if flag:
-                return start
-        return -1
+        start, fuel = 0 , 0
+        for i in range(len(gas)):
+            if gas[i] + fuel < cost[i]:
+                start, fuel = i+1, 0
+            else:
+                fuel += gas[i] - cost[i]
+        return start
 
 gas = [1,2,3,4,5]
 cost = [3,4,5,1,2]
