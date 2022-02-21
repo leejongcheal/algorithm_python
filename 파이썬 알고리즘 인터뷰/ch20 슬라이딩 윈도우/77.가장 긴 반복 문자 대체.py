17b712c43af3b6bcf78b9bf6ced44e18fd422850
lee <aksndk123@naver.com>
2022-02-21 17:19:54 +0900

    * 복습필요
    투포인트 조건문에 따른 이동 정의를 확실하게 하지못함
    res = max(res, end-start + 1)이런식으로 풀필요도 없고

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/77.가장 긴 반복 문자 대체.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/77.가장 긴 반복 문자 대체.py
@@ -6,7 +6,10 @@ class Solution:
         for right in range(len(s)):
             cnt[s[right]] += 1
             max_char_n = cnt.most_common(1)[0][1]
-            # 만족하지 못한경우
+            # 만족하지 못한경우 시작점만 +1 해줌
+            # 왜냐면 만족한경우에는 start를 그대로 둬서 지금 크기보다 큰 슬라이등에
+            # 대해서만 검사를 하는식으로 하면되니깐 -> 복습때 조건만족하지 않을때
+            # 투포인터 이동을 자세히 정의하지못함
             if right + 1 - left - max_char_n > k:
                 cnt[s[left]] -= 1
                 left += 1
