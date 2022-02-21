c6af1042c339679c2d8ee80c2e9c42b718eff77d
lee <aksndk123@naver.com>
2022-02-21 16:54:05 +0900

    ** 조금은 난잡하지만 이런식으로도 짤줄 알아야함
    실수한부분
    check함수에서
    for tt in t:
     if now[tt] == 0: return 0
     이런식으로 했는데 tt의 값이 2개이상인 경우 못잡음
     플러스해서 while문도 start 값이 t의 문자이고 갯수가 1개일때 까지로 했는데 2개 필요한 경우에는 못잡는등 문제많음 -> 카운터를 쓸꺼면 시간상관없이 확실하게 써도 됨
     어쩌피 시간통과함 전의풀이 180 -> 현풀이 680

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/76.부분 문자열이 포함된 최소 윈도우.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/76.부분 문자열이 포함된 최소 윈도우.py
@@ -1,32 +1,31 @@
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        def check(now):
+            ## 처음에는 key가 t의 값이고 1이면 ok로함
+            ## 근데 t = "aa" 인경우에는 a의 갯수가 2개이상이여야함
+            for key in need.keys():
+                if now[key] < need[key]:
+                    return 0
+            return 1
+        res = ""
+        start = 0
         need = Counter(t)
-        missing = len(t)
-        left = start = 0
-        end = -1
-        for right, char in enumerate(s):
-            ## BB AC인 경우에 BB에서 2번 뺴는 경우를 방지해야함
-            missing -= need[char] > 0
-            need[char] -= 1
-
-            if missing == 0:
-                # 위의 경우에선 BBAC가 아닌 BAC까지 오게됨
-                while left < right and need[s[left]] < 0:
-                    need[s[left]] += 1
-                    left += 1
-                if  end == -1 or right - left<= end - start:
-                    start, end = left, right
-                    need[s[left]] += 1
-                    missing += 1
-                    left += 1
-        if end == -1:
-            return ""
-        return s[start:end + 1]
+        now = Counter()
+        for index, char in enumerate(s):
+            now[char] += 1
+            if check(now):
+                # 계속 비교를 함으로써 a가 2개필요한데 a가 5개 있는경우든 상관없이 최소만족index + 1값이 start에 들어감
+                while check(now):
+                    now[s[start]] -= 1
+                    start += 1
+                if not res or len(res) > index - start + 2:
+                    res = s[start-1:index+1]
+        return res
 
 
 # "aa", "a" -> ""// "a", "a" -> "a" 예외처리 ㅋㅋ
 
 s = "ADOBECODEBANC"
 t = "ABC"
-print(Solution().minWindow("a", "a"))
+print(Solution().minWindow("a","aa"))
