f68d3d54f0a7f9c5e35b021316611ee8d8d07c05
lee <aksndk123@naver.com>
2022-02-21 12:34:42 +0900

    ** Counter의 고급 응용
    cnt = Counter([1,2,2,3,3,3])
    cnt = 3:3 2:2 1:1
    cnt[3] -= 1
    cnt = 2:2 3:2 1:1 처럼 순서가 유지는 되지만 실제로 items()로 for문 돌리면 1 2 3의 순으로 나옴
    따라서 갯수의 크기로 받고 싶다면 most_common을 써야하며 갯수가 2개인데 5개의 most_common을 받으면 2개만 나오니 걱정 ㄴㄴ
    
    그리고 갯수가 0개인것을 삭제하고 싶으면
    cnt += Counter()을 해주면됨!!

--- 파이썬 알고리즘 인터뷰/ch21 그리디 알고리즘/80.태스크 스케줄러.py
+++ 파이썬 알고리즘 인터뷰/ch21 그리디 알고리즘/80.태스크 스케줄러.py
@@ -3,32 +3,22 @@ import heapq
 from collections import deque, Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        tasks_cnt = Counter(tasks)
-        heap = []
-        for key, cnt in tasks_cnt.items():
-            heapq.heappush(heap,[-cnt, key])
-        res = ""
-        sub = 0
-        while heap:
-            temp = []
+        counter = Counter(tasks)
+        result = 0
+        while True:
             sub = 0
-            while heap and sub <= n:
-                cnt, key = heapq.heappop(heap)
-                if cnt + 1 != 0:
-                    temp.append([cnt + 1, key])
-                res += key
+            for task, _ in counter.most_common(n+1):
                 sub += 1
-            if temp:
-                while sub <= n:
-                    sub += 1
-                    res += "0"
-            for cnt, key in temp:
-                heapq.heappush(heap,[cnt,key])
-        # print(res)
-        return len(res)
+                result += 1
+                counter[task] -= 1
+                counter += Counter()
+            if not counter:
+                break
+            result += (n + 1 - sub)
+
+        return result
 
 
 
-
-L =  ["A","A","A","B","B","B"]
-print(Solution().leastInterval(L, 0))
+L =  ["A","A","A","B","B","B","B","B"]
+print(Solution().leastInterval(L, 1))
