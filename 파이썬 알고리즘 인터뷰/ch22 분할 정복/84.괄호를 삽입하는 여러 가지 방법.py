3864de5c43f944aff9ab7ba17149e350e9b43808
lee <aksndk123@naver.com>
2022-02-22 11:17:47 +0900

    *** 솔직히 발상도 어렵고 한번 풀려했는데 도저히 모르겠음
    아예 방법론적인 문제

---
+++ 파이썬 알고리즘 인터뷰/ch22 분할 정복/84.괄호를 삽입하는 여러 가지 방법.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def diffWaysToCompute(self, input: str) -> List[int]:
+        def compute(left, right, op):
+            results = []
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+            return results
+
+        if input.isdigit():
+            return [int(input)]
+
+        results = []
+        for index, value in enumerate(input):
+            if value in "-+*":
+                left = self.diffWaysToCompute(input[:index])
+                right = self.diffWaysToCompute(input[index + 1:])
+
+                results.extend(compute(left, right, value))
+        return results
+
+
+s = "2*3-4*5"
+print(Solution().diffWaysToCompute(s))
