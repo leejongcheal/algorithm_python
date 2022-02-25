0d5930f303dff96b4e41802711760e1ae0b3a539
lee <aksndk123@naver.com>
2022-02-25 16:07:46 +0900

    깔끔풀이 - 카운터의 정확한 사용법
    카운터 set 연산
    a|b 같은 key 가 있는경우 min(a,b)로 들어감
    a&b 같은 key 가 있는경우 max(a,b)로 들어감
    answer = intersection / union if union != 0 else 1
    매우 파이썬 다운 풀이

--- 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/5.뉴스 클러스터링.py
+++ 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/5.뉴스 클러스터링.py
@@ -1,58 +1,26 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17677
-
 from collections import Counter
-def plus_AB(A, B):
-    res = 0
-    a = set(A.keys())
-    b = set(B.keys())
-    plus_ab = a | b
-    for element in plus_ab:
-        if element in a and element in b:
-            res += max(A[element], B[element])
-        elif element in a:
-            res += A[element]
-        else:
-            res += B[element]
-    return res
-
-def minus_AB(A, B):
-    res = 0
-    a = set(A.keys())
-    b = set(B.keys())
-    minus_ab = a & b
-    for element in minus_ab:
-        if element in a and element in b:
-            res += min(A[element], B[element])
-        elif element in a:
-            res += A[element]
-        else:
-            res += B[element]
-    return res
-
 def solution(str1, str2):
     answer = 0
     str1 = str1.lower()
-    temp1 = str1
     str2 = str2.lower()
-    temp2 = str2
-    # for char in str1:
-    #     if "a" <= char <= "z":
-    #         temp1 += char
-    # for char in str2:
-    #     if "a" <= char <= "z":
-    #         temp2 += char
-    str1, str2 = [], []
-    for i in range(len(temp1) - 1):
-        if "a" <= temp1[i] <= "z" and "a" <= temp1[i+1] <= "z":
-            str1.append(temp1[i] + temp1[i+1])
-    for i in range(len(temp2) - 1):
-        if "a" <= temp2[i] <= "z" and "a" <= temp2[i + 1] <= "z":
-            str2.append(temp2[i] + temp2[i+1])
-    str1 = Counter(str1)
-    str2 = Counter(str2)
-    plus = plus_AB(str1, str2)
-    minus = minus_AB(str1, str2)
-    if plus == 0 and minus == 0:
-        return 65536
-    else:
-        return int((minus / plus)*65536)
+    L1, L2 = [], []
+    for i in range(len(str1) - 1):
+        a = str1[i:i+2]
+        if a.isalpha():
+            L1.append(a)
+    for i in range(len(str2) - 1):
+        a = str2[i:i+2]
+        if a.isalpha():
+            L2.append(a)
+    str1 = Counter(L1)
+    str2 = Counter(L2)
+    union = sum((str1|str2).values())
+    intersection = sum((str1&str2).values())
+    answer = intersection / union if union != 0 else 1
+    return int(answer * 65536)
+
+
+s1 = 'FRANCE'
+s2 = 'french'
+print(solution(s1,s2))
