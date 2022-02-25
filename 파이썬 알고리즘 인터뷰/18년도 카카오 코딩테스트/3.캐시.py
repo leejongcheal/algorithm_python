1a438ff89b5074fbfb98ecceafd74593257bab86
lee <aksndk123@naver.com>
2022-02-25 14:30:39 +0900

    좀더 깔끔한 풀이
    q = deque(maxlen=3)
    q의 사이즈를 3으로 두고 이상으로 값을 넣는 경우 가장 오래된 값을 pop하고 push함 - 정말 큐로서 동작함
    근데 이건 진짜 너무하긴하네

--- 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/3.캐시.py
+++ 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/3.캐시.py
@@ -4,16 +4,17 @@ def solution(cacheSize, cities):
     answer = 0
     if cacheSize == 0:
         return 5*len(cities)
-    q = deque([])
-    for index, val in enumerate(cities):
+    q = deque(maxlen=cacheSize)
+    for val in cities:
         val = val.lower()
         if val in q:
+            answer += 1
             q.remove(val)
             q.append(val)
-            answer += 1
         else:
-            if q and len(q) >= cacheSize:
-                q.popleft()
-            q.append(val)
             answer += 5
+            q.append(val)
     return answer
+
+L = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
+print(solution(3, L))
