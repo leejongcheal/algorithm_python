980906ba469e1c493d36bc2ad1b9f1f13226f98c
lee <aksndk123@naver.com>
2022-02-25 17:29:32 +0900

    비슷한 풀이 - 틀린 이유만 확인하기
    마지막 "#"갯수를 찾는 문제에서 전에는 "X"의 갯수를 찾았다.
    근데 input값은 A~Z값이 들어가는걸 안보고 풀어서 틀림
    이거 떄문에 1시간정도 괜히 씨름했음.+
    
    ***sum([x.count("X") for x in board]) 하나 때문에 1시간 날림
    
    의미적으로 삭제하기위해 특정문자를 넣는 경우 -> input값 확인해야함 여기서는 input값이 A~Z까지 가능이라 X에 대해서 걸림

--- 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/6.프렌즈4블록.py
+++ 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/6.프렌즈4블록.py
@@ -1,32 +1,40 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17679
 
 def solution(n, m, board):
-    for i in range(n):
-        board[i] = list(board[i])
-    answer = 0
-    while 1:
-        X_set = set()
+    board = [list(x) for x in board]
+    matched = True
+    while matched:
+        matched = []
         for i in range(n-1):
             for j in range(m-1):
-                if board[i][j] != "X":
-                    if board[i][j] == board[i+1][j]\
-                    == board[i][j+1] == board[i+1][j+1]:
-                        X_set.add((i,j))
-                        X_set.add((i+1,j))
-                        X_set.add((i,j+1))
-                        X_set.add((i+1,j+1))
-        if not X_set:
-            break
-        else:
-            answer += len(X_set)
-        for i, j in X_set:
-            board[i][j] = "X"
+                if board[i][j] == board[i+1][j]\
+                == board[i][j+1] == board[i+1][j+1] != "#":
+                    matched.append([i,j])
+        for i, j in matched:
+            board[i][j] = board[i+1][j]= board[i][j+1]\
+                = board[i+1][j+1] = "#"
         for i in range(n-1,-1,-1):
             for j in range(m):
-                if board[i][j] == "X":
+                if board[i][j] == "#":
                     temp = i
-                    while temp >= 0 and board[temp][j] == "X":
+                    while temp >= 0 and board[temp][j] == "#":
                         temp -= 1
                     if temp != -1:
                         board[i][j], board[temp][j] = board[temp][j], board[i][j]
-    return answer
+    return sum([x.count("#") for x in board])
+
+
+# L = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
+L = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+print(solution(4, 5, L))
+
+
+
+
+
+
+
+
+
+
+
