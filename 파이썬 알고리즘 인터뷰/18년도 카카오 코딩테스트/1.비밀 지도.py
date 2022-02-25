00a47f325780a721751b28173d00de8e084d7206
lee <aksndk123@naver.com>
2022-02-25 14:10:25 +0900

    이진수 + 비트연산 발상 + 문자열 처리 + 풀이
    # str.zfill(10) 문자열에 대해서 str의 길이가 10보다 작으면 왼쪽에 남은수만큼 0으로 채워준다.
    # bin(32) = 0b100000
    # 32|31 = 63 숫자에 대해서 비트연산가능

--- 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/1.비밀 지도.py
+++ 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/1.비밀 지도.py
@@ -1,28 +1,17 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17681?language=python3
 def solution(n, arr1, arr2):
-    answer = []
-    L1 = [[0]*n for _ in range(n)]
-    L2 = [[0]*n for _ in range(n)]
-    for i, val in enumerate(arr1):
-        r = val
-        for j in range(n):
-            r, c = r//2, r%2
-            L1[i][n-1-j] = c
-    for i, val in enumerate(arr2):
-        r = val
-        for j in range(n):
-            r, c = r // 2, r % 2
-            L2[i][n-1-j] = c
+    maps = []
     for i in range(n):
-        for j in range(n):
-            if L2[i][j] == 1:
-                L1[i][j] = 1
-    for i in range(n):
-        temp = ""
-        for j in range(n):
-            if L1[i][j]:
-                temp += "#"
-            else:
-                temp += " "
-        answer.append(temp)
-    return answer
+        maps.append(
+        bin(arr1[i]|arr2[i])[2:].zfill(n).replace("1","#").replace("0"," ")
+        )
+    return maps
+
+arr1 = [9, 20, 28, 18, 11]
+arr2 = [30, 1, 21, 17, 28]
+print(solution(5,arr1,arr2))
+
+
+# str.zfill(10) 문자열에 대해서 str의 길이가 10보다 작으면 왼쪽에 남은수만큼 0으로 채워준다.
+# bin(32) = 0b100000
+# 32|31 = 63 숫자에 대해서 비트연산가능
