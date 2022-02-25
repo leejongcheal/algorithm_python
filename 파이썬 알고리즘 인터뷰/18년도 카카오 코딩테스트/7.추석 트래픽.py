b4c9fdf107c3d66f3674d25c16a71a7d5edc6b12
lee <aksndk123@naver.com>
2022-02-25 13:27:20 +0900

    ** 매우 훌룡한 발상을 할수 있는 문제
    전의 깃확인 + (h*3600 + m*60 + s)*1000 + ms 요 발상
    시간을 초의 정수 or ms 기준 정수로 만들어서 비교해서 풀이하는 이 발상이 매우 멋짐
    그외 heapq사용 + 조건문 나누는 방식이 생각해야할게 많았음

--- 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/7.추석 트래픽.py
+++ 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/7.추석 트래픽.py
@@ -1,6 +1,15 @@
 # (h*3600 + m*60 + s)*1000 + ms*1000
 from collections import deque
 import heapq
+def get_times(datetime, prosessing_log):
+    h = int(datetime[0:2])
+    m = int(datetime[3:5])
+    s = int(datetime[6:8])
+    ms = int(datetime[9:12])
+    processing_time = float(prosessing_log[:-1])
+    end = (h * 3600 + m * 60 + s) * 1000 + ms
+    start = end - (int(processing_time * 1000) - 1)
+    return [start, end]
 
 def solution(lines):
     # [start, end]값을 원소로
@@ -8,27 +17,23 @@ def solution(lines):
     # start end 값 가져오기
     for line in lines:
         log = line.split()
-        datetime = log[1]
-        h = int(datetime[0:2])
-        m = int(datetime[3:5])
-        s = int(datetime[6:8])
-        ms = int(datetime[9:12])
-        processing_time = float(log[2][:-1])
-        end = (h*3600 + m*60 + s)*1000 + ms
-        start = end - (int(processing_time*1000) - 1)
-        L.append([start, end])
-    # if len(L) == 1:
-    #     return 1
+        L.append(get_times(log[1], log[2]))
+    # 완료시간순으로 인풋값이 들어오니
+    time_end = L[-1][1]
+    # time_end = sorted(L, key=lambda x: -x[1])[0][1]
     L.sort()
-    time_start = L[0][0]
-    time_end = sorted(L, key=lambda x:-x[1])[0][1]
-    time = time_start
+    time = L[0][0]
     answer = 0
     now_request = 0
     q = []
     heapq.heappush(q,L[now_request][1])
     now_request = 0
-    while time <= time_end:
+    # 좀더 최적화 할수 있는데 굳이 그러고 싶지않음
+    # 최적화함 4.002~5.001 을 1초로 하니 첫번째 조건문
+    # start + 1000 > end 인 경우를 생각해서 2번째 조건문도 넣어줌
+    while time <= time_end - 999 or time <= L[-1][0]:
+        # 로그가 하나인 경우를 위해 빈큐가 아닌 1번째 원소가 들어간 큐로 초기화
+        # re+1 < len(L)을 확인할 필요가 없는 이유는  time <= time_end에서 걸러지기 때문이다.
         if not q:# and now_request + 1 < len(L):
             time = L[now_request + 1][0]
             heapq.heappush(q,(L[now_request+1][1]))
@@ -56,7 +61,10 @@ L = [
 "2016-09-15 21:00:00.966 0.381s",
 "2016-09-15 21:00:02.066 2.62s"
 ]
-# L =  [
-# "2016-09-15 01:00:04.002 2.0s"
-# ]
+# L = [
+# "2016-09-15 00:00:01.000 0.9s",
+# "2016-09-15 00:00:02.000 0.9s",
+# "2016-09-15 00:00:03.000 0.9s",
+# "2016-09-15 00:00:04.000 2.999s",
+#     ]
 print(solution(L))
