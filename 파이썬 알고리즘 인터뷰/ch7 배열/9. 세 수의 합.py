78962a39ae187d8be76b00912fed55f74114360b
lee <aksndk123@naver.com>
2022-02-28 16:53:47 +0900

    ** 투포인트 는 잘했는데 중복제거에서 생각 모자람 복습

--- 파이썬 알고리즘 인터뷰/ch7 배열/9. 세 수의 합.py
+++ 파이썬 알고리즘 인터뷰/ch7 배열/9. 세 수의 합.py
@@ -4,35 +4,36 @@ from itertools import combinations
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3:
-            return []
-        # print(nums)
-        nums.sort()
         res = []
-        # print(nums)
-        for k in range(len(nums) - 2):
-            # print(k)
-            if k > 0 and nums[k] == nums[k - 1]:
+        nums.sort()
+        prev = -(1e10)
+        for i in range(len(nums)-2):
+            # 중복 제거위함
+            if prev == nums[i]:
                 continue
-            left = k + 1
-            right = len(nums) - 1
-            # print(right)
+            prev = nums[i]
+            left, right = i + 1, len(nums) - 1
             while left < right:
-                print(left, right)
-                sum = nums[k] + nums[left] + nums[right]
-                if sum < 0:
-                    left += 1
-                elif sum > 0:
-                    right -= 1
-                else:
-                    res.append([nums[k], nums[left], nums[right]])
+                now = nums[i] + nums[left] + nums[right]
+                if now == 0:
+                    res.append([nums[i], nums[left], nums[right]])
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
+                    # 요기를 잘못생각함
                     left += 1
+                    right -= 1
+                elif now < 0:
+                    left += 1
+                elif now > 0:
                     right -= 1
         return res
 
 
-print(Solution().threeSum([-1, 0, 1, 2, -1, -4]), "CC")
+
+
+
+
+print(Solution().threeSum([-2,0,0,2,2]), "CC")
+# print(Solution().threeSum([-1, 0, 1, 2, -1, -4, -1]), "CC")
