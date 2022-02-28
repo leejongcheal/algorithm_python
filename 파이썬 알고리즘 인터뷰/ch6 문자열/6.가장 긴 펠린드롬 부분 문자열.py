ccc66fa5b23db8f903433f2e6364d9135b3bc849
lee <aksndk123@naver.com>
2022-02-28 15:11:41 +0900

    ** 투포인터 응용 와우 max(a,b,c,key=len)방식 기억
    왼쪽과 오른쪽이 이동하는걸 각각 -1 +1 로 해서 풀었음
    -> 나는 무조건 앞으로 가는거만 생각함
    ***슬라이딩 앞으로
    ***투포인터 왼쪽은 -1 오른쪽은 +1 방식
    
    그리고 짝수인 경우 홀수 인경우 나누어서 푸는 생각을 못함 + 펠린드롬 판별하는 함수를 없애고 left 와 right만 비교하는 방식으로 개선
    전꺼보다 10배 빨라짐

--- 파이썬 알고리즘 인터뷰/ch6 문자열/6.가장 긴 펠린드롬 부분 문자열.py
+++ 파이썬 알고리즘 인터뷰/ch6 문자열/6.가장 긴 펠린드롬 부분 문자열.py
@@ -1,19 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def check(strs):
-            return strs == strs[::-1]
+        def expand(left, right):
+            if right >= len(s):
+                return ""
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
         res = s[0]
-        for start in range(len(s)):
-            end = start + len(res)
-            while end < len(s):
-                if check(s[start:end+1]):
-                    if len(res) < end - start + 1:
-                        res = s[start:end+1]
-                end += 1
+        for i in range(len(s)):
+            res = max(expand(i,i+1), expand(i,i+2), res,key=len)
         return res
 
 
 
 
-strs = "babad"
+strs = "acd"
 print(Solution().longestPalindrome(strs))
