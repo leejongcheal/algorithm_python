bc20c48e6e0b1d1445e40fecd92a61143704c6bf
lee <aksndk123@naver.com>
2022-02-28 15:46:36 +0900

    * 복습 투포인터로 풀려하니 [3,3] 6때문에 더 복잡한 무언가가 필요함
    [3,3] 6 예외같은 상황을 이해하고 넘기자

--- 파이썬 알고리즘 인터뷰/ch7 배열/7.두 수의합.py
+++ 파이썬 알고리즘 인터뷰/ch7 배열/7.두 수의합.py
@@ -1,26 +1,25 @@
 from typing import List
 
 
+# [3,3] 6인 경우 이렇게 풀면 복잡하게 연산이 있어야함
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = dict()
         for i,val in enumerate(nums):
             dic[val] = i
-        for i, num in enumerate(nums):
-            complement = target - num
-            ## [3,2,4] 6 인경우 3 3도 답일수 있으니 이에대한 예외처리 필요
-            if complement in dic and i != dic[complement]:
-                return [i, dic[complement]]
-        # nums.sort()
-        # start, end = 0, len(nums) - 1
-        # while start < end:
-        #     now = nums[start] + nums[end]
-        #     if now == target:
-        #         return [start, end]
-        #     elif now < target:
-        #         start += 1
-        #     elif now > target:
-        #         end -= 1
+        nums.sort()
+        start, end = 0, len(nums) - 1
+        while start < end:
+            now = nums[start] + nums[end]
+            if now == target:
+                if dic[nums[start]] != dic[nums[end]]:
+                    a = min(dic[nums[start]],dic[nums[end]])
+                    b = max(dic[nums[start]],dic[nums[end]])
+                    return [a, b]
+            elif now < target:
+                start += 1
+            elif now > target:
+                end -= 1
 
 L = [3,2,4]
 print(Solution().twoSum(L,6))
