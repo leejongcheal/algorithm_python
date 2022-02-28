dca8d88f09ed044bd85186aa5059ee23982794e1
lee <aksndk123@naver.com>
2022-02-28 22:24:38 +0900

    ** 문제이해후 혼자힘으로 푼 코드
    솔직히 틀린 예제 다주니깐 알고리즘 확실하게 꺠달아서 풀었음
    예제없이 혼자하라한다? 가능했을까 싶긴함
    너무 복잡하게 풀었나 ㅋㅋ fix and temp두고 푸는 테크닉은 내가 생각해도 대단

--- 파이썬 알고리즘 인터뷰/ch9 스택, 큐/21.중복 문자 제거.py
+++ 파이썬 알고리즘 인터뷰/ch9 스택, 큐/21.중복 문자 제거.py
@@ -1,16 +1,40 @@
 from collections import Counter
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        counter, seen, stack = Counter(s), set(), []
+        fix = []
+        temp = []
+        counter = Counter(s)
         for char in s:
-            counter[char] -= 1
-            if char in seen:
+            # 이미 fix에 있는 경우는 무시
+            if char in fix:
                 continue
-            while stack and char < stack[-1] and counter[stack[-1]] > 0:
-                seen.remove(stack.pop())
-            stack.append(char)
-            seen.add(char)
-        return "".join(stack)
+            # 이미 temp에 있는경우 앞에 있는자리가 강력한 후보자리임
+            # aba일때의 경우 temp a,b 이고 a의 갯수가 0이되는경우 체크
+            elif char in temp:
+                counter[char] -= 1
+            # temp가 비거나 char 큰경우 마지막으로
+            elif not temp or temp[-1] < char:
+                temp.append(char)
+                counter[char] -= 1
+            # temp가 있을때, temp에 char보다 큰건 빼버리고 char을 넣어줌
+            elif temp and temp[-1] > char:
+                while temp and temp[-1] > char:
+                    temp.pop()
+                temp.append(char)
+                counter[char] -= 1
+            # 일단 갯수가 0이되면 자리를 확정해야함 temp 1,2,3,4 일때 현재 3이고 0의 갯수가 되면
+            # 3의 자리를 확정해야하고 사전순을 지키기위해 1,2,3만 fix로 가야함
+            if counter[char] == 0:
+                index = temp.index(char)
+                fix += temp[:index + 1]
+                temp = temp[index + 1:]
+        # 나머지 자리 확정못한 것들을 가능한 사전순으로 fix에 넣어줌
+        fix += temp
+        return "".join(fix)
 
-
-print(Solution().removeDuplicateLetters("cbacdcbc"))
+# "cbacdcbc" "acdb"
+# "bddbccd" "bcd"
+# "bbcaac" "bac"
+# "cdadabcc" "adbc"
+s = "cbacdcbc"
+print(Solution().removeDuplicateLetters(s))
