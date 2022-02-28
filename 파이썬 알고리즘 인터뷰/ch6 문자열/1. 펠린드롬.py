c502da017d45cb377faff3fb85a715be05fcf79b
lee <aksndk123@naver.com>
2022-02-28 13:20:38 +0900

    * 문자열 관련해서 기본적인 처리함수들 리트코트형태로 바꾼 코드
    s.lower()이 메서드가 아니라 함수느낌으로
    s = s.lower() 이런식으로 사용해야함 --> 아니 말이됨?
    char.inalnum() === '0' <= char <= '9' or
    'a' <= char <= 'z' or "A" <= char <= "Z"
    정규표현식
    import re
    text_temp = re.sub("패턴","대체할 문자",해당 문자열)
    패턴에는 그냥 "aa|bb" 처럼 aa or bb 를 대체할 문자로 바꾸는것처럼 쓰거나 정규표현식을 사용할수 있다.
    ex) text_temp = re.sub('[^a-zA-Z0-9]'',"",text)
    text에 대해서 숫자 + 알파벳인 문자를 ""빈문자열로 대체해서 반환
    re.findall("[\w]", s)
    s에 대해서 숫자 + 알파벳 == \w 인 경우 리스트에 추가해서 리스트로 반환

--- 파이썬 알고리즘 인터뷰/ch6 문자열/1. 펠린드롬.py
+++ 파이썬 알고리즘 인터뷰/ch6 문자열/1. 펠린드롬.py
@@ -1,13 +1,9 @@
 import re
-s = input()
-s = s.lower()
-# 1
-# 문자에 대해서 char.isalnum()을 이용해서 숫자 or 문자인지 판별 가능
-# 2
-s = re.sub('[^a-z0-9]','',s)
-# \w 는 숫자 + 알파벳문자를 뜻함 [a-zA-Z0-9]
-# s = re.sub("[\W]","",s)
-# 3
-# L_s = re.findall("[\w]", s)
-# s = "".join(L_s)
-print(s == s[::-1])
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub("[^a-z0-9]", "", s)
+        return s == s[::-1]
+
+s = "ab_a"
+print(Solution().isPalindrome(s))
