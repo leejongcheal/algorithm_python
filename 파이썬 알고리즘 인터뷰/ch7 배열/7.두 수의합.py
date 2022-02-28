a77acf57fd125576a91a5fff282c22f313051040
lee <aksndk123@naver.com>
2022-02-28 15:40:12 +0900

    * 복습 투포인터로 풀려했는데 틀리는 이유 생각
    nums가 정렬된 상태가 아니고, 정렬해서 사용을 해도 index값이 필요한 문제이므로 투포인터로 풀려면 좀더 연산이 필요함
    일단 사전으로 편하게 푸는 방법으로 풀어봄

--- 파이썬 알고리즘 인터뷰/ch7 배열/7.두 수의합.py
+++ 파이썬 알고리즘 인터뷰/ch7 배열/7.두 수의합.py
@@ -1,13 +1,26 @@
-L = list(map(int,input().split()))
-target = int(input())
-# index를 이용한 풀이 -> 리스트에 find메서드는 없다
-# for i, now in enumerate(L):
-#     complement = target - now
-#     if complement in L[i+1:]:
-#         print(i, L.index(complement))
-nums_map = {}
-for i, now in enumerate(L):
-    complemt = target - now
-    if complemt in nums_map:
-        print(nums_map[complemt], i)
-    nums_map[now] = i
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = dict()
+        for i,val in enumerate(nums):
+            dic[val] = i
+        for i, num in enumerate(nums):
+            complement = target - num
+            ## [3,2,4] 6 인경우 3 3도 답일수 있으니 이에대한 예외처리 필요
+            if complement in dic and i != dic[complement]:
+                return [i, dic[complement]]
+        # nums.sort()
+        # start, end = 0, len(nums) - 1
+        # while start < end:
+        #     now = nums[start] + nums[end]
+        #     if now == target:
+        #         return [start, end]
+        #     elif now < target:
+        #         start += 1
+        #     elif now > target:
+        #         end -= 1
+
+L = [3,2,4]
+print(Solution().twoSum(L,6))
