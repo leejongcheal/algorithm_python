fcf383acf2bfc4114ba05eef6258b9ed3eb91db5
lee <aksndk123@naver.com>
2022-02-28 15:15:35 +0900

    *** 조오금더 개선된 방식
    예외처리 s == s[::-1] 인경우 바로 s 반환
    s[left + 1, right] 인 부분을 이용해서 "ab"같은 값은 알아서 b로 처리됨 (i=0인경우)
    전보다 4배 개선된 풀이

--- 파이썬 알고리즘 인터뷰/ch6 문자열/6.가장 긴 펠린드롬 부분 문자열.py
+++ 파이썬 알고리즘 인터뷰/ch6 문자열/6.가장 긴 펠린드롬 부분 문자열.py
@@ -1,13 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def expand(left, right):
-            if right >= len(s):
-                return ""
+            # if right >= len(s):
+            #     return ""
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
+            # "ac" 인경우에 0,2가 들어오면 s[1:2]로 c 반환됨
             return s[left+1:right]
-        res = s[0]
+        # res = s[0]
+        res = ""
+        # "z" 인경우 z반환하기 위한 예외처리 + s자체가 펠린인경우 시간절약
+        if len(s) < 2 or s == s[::-1]:
+            return s
         for i in range(len(s)):
             res = max(expand(i,i+1), expand(i,i+2), res,key=len)
         return res
