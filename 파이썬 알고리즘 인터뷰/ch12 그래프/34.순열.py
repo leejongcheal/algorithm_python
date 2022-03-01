35b4dd70edae33b2796103081824fb9c10f67fcc
lee <aksndk123@naver.com>
2022-03-01 13:01:59 +0900

    ** DFS부분에서 개선할 부분 암기수준으로보기
    if n not in path: 이부분을 visit을 사용해서 처리하자
    visit[index] = 1
    dfs(...)
    visit[index] = 0
    요런식으로 푸는거 dfs -> visit써야한다랑 저런 방식은 암기

--- 파이썬 알고리즘 인터뷰/ch12 그래프/34.순열.py
+++ 파이썬 알고리즘 인터뷰/ch12 그래프/34.순열.py
@@ -3,7 +3,17 @@ from itertools import permutations
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(map(list,permutations(nums, len(nums))))
+        res = []
+        def dfs(path):
+            if len(path) == len(nums):
+                res.append(path)
+                return
+            for n in nums:
+                # 이부분을 visit 으로 처리하면 2배 빨라짐 -> 이거를 상기**
+                if n not in path:
+                    dfs(path + [n])
+        dfs([])
+        return res
 
 
 print(Solution().permute([1,2,3]))
