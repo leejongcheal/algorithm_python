fc6dc146a6bb5d9c880206c4553f3d2063396147
lee <aksndk123@naver.com>
2022-03-01 16:19:41 +0900

    **** 의미적으로 완벽한 풀이
    dist+_list[(index, k)] 이 방식 훌륭함

--- 파이썬 알고리즘 인터뷰/ch13 최단 경로 문제/41.K 경유지 내 가장 저렴한 항공권.py
+++ 파이썬 알고리즘 인터뷰/ch13 최단 경로 문제/41.K 경유지 내 가장 저렴한 항공권.py
@@ -4,36 +4,33 @@ from collections import defaultdict
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
         INF = int(1e10)
+        dist_list = defaultdict(int)
         graph = defaultdict(list)
-        for a,b,w in flights:
-            graph[a].append((b,w))
-        res = [INF]*(n)
-        res[src] = 0
-        q = [(0, src, 0)]
-        visit = set()
+        res = [] # k개 이내에 dist에 도착한 경우에 추가
+        q = [(0, src, -1)]
+        for a,b,c in flights:
+            graph[a].append((b, c))
         while q:
-            now_dist, now, k = heapq.heappop(q)
-            if (now, k) in visit:
+            dist, now, k = heapq.heappop(q)
+            if k > K or (dist_list[(now, k)] != 0 and dist_list[(now, k)] < dist):
                 continue
-            visit.add((now, k))
-            # 결국엔 dst를 제일 먼저 오는것은 k내의 제일 작은 dist를 가진값이므로 이런식으로 리턴 해도 정답임
-            # 위처럼 생각했는데 더더욱 복잡해지는 경우 답이 아닐수도 있음 -> 리트코드는 통과되지만 수정
-            # if now == dst:
-            #     return now_dist
+            # k는 위에서 걸리는 조사할 필요없고, 한번이라도 나오는경우가 제일 작은값이니 바로 return해준다.
             if now == dst:
+                res.append(dist)
                 continue
-            # 현재 k는 다음에 방문할 노드의 현재 경유지 갯수를 뜻함 -> 따라서 조건문 전에 dst이면 상관없음
-            if k > K:  # res[now] < now_dist일 경우도 돌아야함 k 기준으로 탐색해야하니
+            # 이렇게 생략할려고 했는데 복잡해지는경우 답이 아닐수도 있음
+            # if now == dst:
+            #     return dist
+            if dist < dist_list[(now, k)]:
                 continue
-            for next, dist in graph[now]:
-                next_dist = dist + now_dist
-                if res[next] > next_dist:
-                    res[next] = next_dist
-                # 안쪽일 경우에만 값이 제일 작고 k내일때 res 수정
-                # 검사는 k의 기준에 따라서 작은값을 넣어주는식으로 탐색
-                heapq.heappush(q,(next_dist, next, k + 1))
-        if res[dst] != INF:
-            return res[dst]
+            for next, val in graph[now]:
+                if dist_list[(next, k+1)] == 0 or dist + val < dist_list[(next, k+1)]:
+                    dist_list[(next, k+1)] = dist + val
+                    heapq.heappush(q, (dist_list[(next, k+1)], next, k + 1))
+        if res:
+            return min(res)
         else:
             return -1
-print(Solution().findCheapestPrice(5, [[0,1,5],[1,2,5],[0,3,2],[3,1,2],[1,4,1],[4,2,1]],0,2,2))
+# 5, [[0,1,5],[1,2,5],[0,3,2],[3,1,2],[1,4,1],[4,2,1]],0,2,2 // 7
+# 5,[[1,2,10],[2,0,7],[1,3,8],[4,0,10],[3,4,2],[4,2,10],[0,3,3],[3,1,6],[2,4,5]],0,4,1 //  5
+print(Solution().findCheapestPrice(5,[[1,2,10],[2,0,7],[1,3,8],[4,0,10],[3,4,2],[4,2,10],[0,3,3],[3,1,6],[2,4,5]],0,4,1))
