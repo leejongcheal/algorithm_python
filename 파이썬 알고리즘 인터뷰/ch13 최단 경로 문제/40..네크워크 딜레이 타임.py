ec0b5e37594564a4941aeca3d548411f6711fd38
lee <aksndk123@naver.com>
2022-03-01 15:44:20 +0900

    * 다익스트라 수정 코드
    다중방문 처리를 안해서 추가해줌
    res에 -1값을 넣어서 사용했는데 INF로 푸는게 더 의미적으로 맞음
    if dist > res[now]:
       continue
    이 부분은 q에 now값이 여러개들어가는 경우가 생길수도 있는데 이를 방지하기 위한 코드임 -> 까먹고 못써서 수정함

--- 파이썬 알고리즘 인터뷰/ch13 최단 경로 문제/40..네크워크 딜레이 타임.py
+++ 파이썬 알고리즘 인터뷰/ch13 최단 경로 문제/40..네크워크 딜레이 타임.py
@@ -3,27 +3,24 @@ import heapq
 from collections import defaultdict
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        INF = int(1e10)
         graph = defaultdict(list)
-        for a, b, c in times:
-            graph[a].append((b,c))
-        res = [-1]*(n+1)
-        res[k] = 0
-        q = []
-        heapq.heappush(q,(res[k],k))
+        for a,b,c in times:
+            graph[a].append((c, b))
+        res = [INF]*(n+1)
+        res[0], res[k] = 0, 0
+        q = [(0, k)]
         while q:
-            now_dist, now = heapq.heappop(q)
-            if now_dist < res[now]:
+            dist, now = heapq.heappop(q)
+            # 다익스트라 다중방문 제거
+            if dist > res[now]:
                 continue
-            for next, dist in graph[now]:
-                next_dist = dist + now_dist
-                if res[next] == -1 or next_dist< res[next]:
-                    res[next] = next_dist
-                    heapq.heappush(q,(next_dist, next))
-        if -1 in res[1:n+1]:
+            for val, next in graph[now]:
+                if dist + val < res[next]:
+                    res[next] = dist + val
+                    heapq.heappush(q,(res[next], next))
+        if INF in res:
             return -1
-        return max(res[1:n+1])
-
+        return max(res)
 print(Solution().networkDelayTime([[2,1,1],[2,3,1],[3,4,1]],4,2))
 print(Solution().networkDelayTime([[1,2,1]],2,2))
-
-
