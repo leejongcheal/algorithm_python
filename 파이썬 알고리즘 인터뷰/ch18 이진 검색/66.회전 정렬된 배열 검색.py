3ce8004f6325f19c4edaa913fe504de9a6e3cd91
lee <aksndk123@naver.com>
2022-03-02 06:39:58 +0900

    ** pivot을 이용할때 주의점 상기
    return pivot + mid하면 안되는걸 생각하면서 풀어야함

--- 파이썬 알고리즘 인터뷰/ch18 이진 검색/66.회전 정렬된 배열 검색.py
+++ 파이썬 알고리즘 인터뷰/ch18 이진 검색/66.회전 정렬된 배열 검색.py
@@ -1,31 +1,18 @@
 from typing import List
+from bisect import bisect_left
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # 피봇(제일 작은값 찾는 과정)
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] > nums[right]:
-                left = mid + 1
-            elif nums[mid] <= nums[right]:
-                right = mid
-        pivot = left
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            mid_pivot = (mid + pivot) % len(nums)
-            if nums[mid_pivot] < target:
-                left = mid + 1
-            elif nums[mid_pivot] > target:
-                right = mid - 1
-            else:
-                return mid_pivot
-        return -1
-
-
-
+        pivot_value = min(nums)
+        pivot = nums.index(pivot_value)
+        L = nums[pivot:] + nums[:pivot]
+        index = bisect_left(L, target)
+        if index < len(nums) and L[index] == target:
+            return (pivot + index) % len(nums)
+        else:
+            return -1
 
 nums = [4,5,6,7,0,1,2]
+# nums = [1]
 target = 0
-
-
+print(Solution().search([3,1], 3))
