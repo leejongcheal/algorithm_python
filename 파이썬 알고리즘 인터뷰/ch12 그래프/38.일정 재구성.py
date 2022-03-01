a1213942c093429a5e06abcb1bedf27e99db3c0e
lee <aksndk123@naver.com>
2022-03-01 14:09:13 +0900

    **** 경로를 기준으로 visit 처리 + 경로가 두번나올수도 있는경우 visit 처리
    와 내가 풀었는데 매우 멋진 풀이 이렇게 푸는게 말이됨
    일단 사전의 key값으로 리스트가 못들어가니 원소를 튜플로 만드는 연산
    tickets = list(map(tuple, tickets)) 때려주기
    이때 list를 빼버리면 안되는거 무조건 인지하기
    후에 counter = Counter(tickets)
    을 쓰고 visit[(now, next)] < counter[(now, next)]인 경우 dfs가도록 풀이
    
    경로를 기준으로 체크해야되는거를 발상하고, 경로가 두번 나오는 경우도 있다는걸 알아야함

--- 파이썬 알고리즘 인터뷰/ch12 그래프/38.일정 재구성.py
+++ 파이썬 알고리즘 인터뷰/ch12 그래프/38.일정 재구성.py
@@ -1,27 +1,31 @@
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, Counter
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        def dfs(path):
-            now = path[-1]
-            if len(path) == N:
-                return path[:]
-            temp = graph[now][:]
-            for next in temp:
-                graph[now].remove(next)
-                a = dfs(path + [next])
-                if a is not None:
-                    return a
-                graph[now].append(next)
-                graph[now].sort()
+        dic = defaultdict(list)
+        N = len(tickets)
+        # 그냥 map으로 쓰니깐 문제생기는데 list를 뺴먹었음
+        tickets = list(map(tuple, tickets))
+        visit_cnt = defaultdict(int)
+        counter = Counter(tickets)
+        for start, end in sorted(tickets):
+            dic[start].append(end)
+
+        def dfs(now, path=["JFK"]):
+            if len(path) == N + 1:
+                return path
+            for next in dic[now]:
+                if visit_cnt[(now, next)] < counter[(now, next)]:
+                    visit_cnt[(now, next)] += 1
+                    q = dfs(next, path + [next])
+                    visit_cnt[(now, next)] -= 1
+                    if q:
+                        return q
             return None
-        tickets.sort()
-        graph = defaultdict(list)
-        N = len(tickets) + 1
-        for a, b in tickets:
-            graph[a].append(b)
-        return dfs(["JFK"])
+        return dfs("JFK")
 
 
-
-print(Solution().findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
+L = [["EZE","AXA"],["TIA","ANU"],["ANU","JFK"],["JFK","ANU"],
+ ["ANU","EZE"],["TIA","ANU"],["AXA","TIA"],["TIA","JFK"],["ANU","TIA"],["JFK","TIA"]]
+print(sorted(L))
+print(Solution().findItinerary(L))
