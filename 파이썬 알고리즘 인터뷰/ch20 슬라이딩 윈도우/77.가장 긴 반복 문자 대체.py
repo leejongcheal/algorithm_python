059eb2f16937648379dcdb64e0cfc54bb457c1b1
lee <aksndk123@naver.com>
2022-03-02 11:18:38 +0900

    * 제일 시간빠른 코드
    1. 카운터를 안쓰는 방식
    1.5 카운터는 쓰고 most_commos는 안쓰는 방식 -> defaultdict로 대체
    2. size고정안하고 푸는 풀이
    
    ^1 ^2 -> 550
    ^1 2 -? 450
    1 ^2 -> 180
    1 2 -> 90 순으로 시간개선
    1.5 2 -> 100 사실상 most_commons가 문제였음
    -> 카운터에서 시간개선 가능한데 most_commons에서 시간좀 걸리는듯
    most_commons자체가 nlogn의 시간복잡도를 가짐

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/77.가장 긴 반복 문자 대체.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/77.가장 긴 반복 문자 대체.py
@@ -1,19 +1,14 @@
-from collections import Counter
+from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
-        mapper = {}
+        mapper = defaultdict(int)
         max_repeat = 0
         # max_length = 0
         for right, letter in enumerate(s):
-            # counter를 안쓰는 경우 속도가 훨씬 빠름 -> 근데 왜?
-            if letter in mapper:
-                mapper[letter] += 1
-            else:
-                mapper[letter] = 1
+            # counter를 안쓰는 경우 속도가 훨씬 빠름 -> 근데 왜? -> most_commos가 문제
+            mapper[letter] += 1
             max_repeat = max(max_repeat, mapper[letter])
-            # 여길 while문 안돌고 그냥 만족못하면 left += 1만 하는식으로
-            # 전의 size는 유지하면서 이동
             if right - left + 1 - max_repeat > k:
                 mapper[s[left]] -= 1
                 left += 1
