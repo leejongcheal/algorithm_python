a1a801e9646741bea555adc6a118bd9cc2bf31a6
lee <aksndk123@naver.com>
2022-03-02 11:04:41 +0900

    그냥 알고리즘 아는 상태에서 짠 코드
    while문을 쓰면 counter값을 수정하면서 풀었는데....
    슬라이딩 size를 유지하면서 더큰 size를 찾으면 그 size로 다시 검사하는식으로 풀어야하는데, 나의풀이는 사실상 다풀어보는 풀이임

--- 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/77.가장 긴 반복 문자 대체.py
+++ 파이썬 알고리즘 인터뷰/ch20 슬라이딩 윈도우/77.가장 긴 반복 문자 대체.py
@@ -2,19 +2,17 @@ from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
-        cnt = Counter()
-        for right in range(len(s)):
-            cnt[s[right]] += 1
-            max_char_n = cnt.most_common(1)[0][1]
-            # 만족하지 못한경우 시작점만 +1 해줌
-            # 왜냐면 만족한경우에는 start를 그대로 둬서 지금 크기보다 큰 슬라이등에
-            # 대해서만 검사를 하는식으로 하면되니깐 -> 복습때 조건만족하지 않을때
-            # 투포인터 이동을 자세히 정의하지못함
-            if right + 1 - left - max_char_n > k:
-                cnt[s[left]] -= 1
+        c = Counter()
+        res = 0
+        for right, val in enumerate(s):
+            c[val] += 1
+            while right - left + 1 - c.most_common(1)[0][1] > k:
+                c[s[left]] -= 1
                 left += 1
-        return right + 1 - left
+            res = max(res, right - left + 1)
+        return res
 
-s = "AAABBC"
-print(Solution().characterReplacement("A",0))
+s = "AAABCBCAABBBBCCC"
+# print(Solution().characterReplacement("A",0))
+print(Solution().characterReplacement(s,2))
 
