f5d9bc468cab8de07b9853c97c3db983e6965d8f
lee <aksndk123@naver.com>
2022-03-03 10:28:36 +0900

    * 조건문 처리 더 의미적으로 코딩 수정

--- 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/4.셔틀 버스.py
+++ 파이썬 알고리즘 인터뷰/18년도 카카오 코딩테스트/4.셔틀 버스.py
@@ -10,34 +10,27 @@ def get_hhmm(val : int):
     return str(h).zfill(2)+":"+str(m).zfill(2)
 # 분을 기준으로 시간을 정수화 시키자
 def solution(n, t, m, timetable):
-    person = []
-    bus = [[9*60,m]]
-    for i in range(n-1):
-        bus.append([bus[-1][0]+t, m])
-    for t in sorted(timetable):
-        person.append(get_m_int(t))
-    bus_index = 0
-    person_index = 0
-    while 1:
-        if bus_index >= n or person_index >= len(person):
-            break
-        p = person[person_index]
-        b = bus[bus_index]
-        if p <= b[0]:
-            if bus_index == n - 1 and b[1] == 1:
-                print(bus)
-                return get_hhmm(p - 1)
-            else:
-                b[1] -= 1
-                if not b[1]:
-                    bus_index += 1
-            person_index += 1
+    person_list = []
+    for time in timetable:
+        person_list.append(get_m_int(time))
+    person_list.sort()
+    start = 9 * 60
+    bus_list = []
+    for i in range(n):
+        bus_list.append([start + t*i, m])
+    now = 0
+    for person in person_list:
+        if person <= bus_list[now][0] and bus_list[now][1] > 0:
+            bus_list[now][1] -= 1
         else:
-            bus_index += 1
-    # 어쩌피 위에서 걸러지는 경우이니 여기까지 오면 막차에 자리가 무조건 비어있다.
-    # if bus[-1][1] > 0:
-    return get_hhmm(bus[-1][0])
-
+            while now < len(bus_list) and not (person <= bus_list[now][0] and bus_list[now][1] > 0):
+                now += 1
+            if now >= len(bus_list):
+                break
+            bus_list[now][1] -= 1
+        if now == len(bus_list) - 1 and bus_list[now][1] == 0:
+            return get_hhmm(person - 1)
+    return get_hhmm(bus_list[-1][0])
 
 L =["09:10", "09:09", "08:00"]
 print(solution(2,10,2,L))
